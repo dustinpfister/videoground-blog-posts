ecc62b12c5036ae59c47d88e0ea6e6d7461a81e9
dustinpfister <dustin.pfister@gmail.com>
2022-03-07 15:45:41 -0500

    floor mesh

--- videos/threejs-fog/fog-basic.js
+++ videos/threejs-fog/fog-basic.js
@@ -15,6 +15,13 @@ VIDEO.init = function(sm, scene, camera){
             color: new THREE.Color('red')
         }));
     scene.add(mesh);
+    let floor = scene.userData.floor = new THREE.Mesh(
+        new THREE.BoxGeometry(30, 30, 1),
+        new THREE.MeshStandardMaterial({
+            color: new THREE.Color('green')
+        }));
+    floor.rotation.x = 1.57;
+    scene.add(floor);
     // LIGHT
     let light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(8, 2, -1).normalize();
@@ -24,6 +31,7 @@ VIDEO.init = function(sm, scene, camera){
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
     let mesh = scene.userData.mesh;
+    mesh.position.y = 1.5;
     mesh.position.z = -8 + 11 * sm.bias;
 };
 
--- videos/threejs-fog/fog-values.js
+++ videos/threejs-fog/fog-values.js
@@ -2,19 +2,27 @@
 VIDEO.init = function(sm, scene, camera){
     var fogColor = new THREE.Color('white');
     scene.background = fogColor;
-    scene.fog = new THREE.Fog(fogColor, 0.25, 15);
+    scene.fog = new THREE.Fog(fogColor, 0.25, 1);
     // CAMERA
     camera.position.set(0, 3, 8);
     camera.lookAt(0, 0, 0);
     // GRID HELPER
     scene.add(new THREE.GridHelper(8, 8));
     // MESH
-    let mesh = scene.userData.mesh = new THREE.Mesh(
+    let sphere = scene.userData.sphere = new THREE.Mesh(
         new THREE.SphereGeometry(1, 30, 30),
         new THREE.MeshStandardMaterial({
             color: new THREE.Color('red')
         }));
-    scene.add(mesh);
+    sphere.position.y = 1.5;
+    scene.add(sphere);
+    let floor = scene.userData.floor = new THREE.Mesh(
+        new THREE.BoxGeometry(30, 30, 1),
+        new THREE.MeshStandardMaterial({
+            color: new THREE.Color('green')
+        }));
+    floor.rotation.x = 1.57;
+    scene.add(floor);
     // LIGHT
     let light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(8, 2, -1).normalize();
@@ -23,7 +31,6 @@ VIDEO.init = function(sm, scene, camera){
 
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
-    let mesh = scene.userData.mesh;
-    scene.fog.far = 1 + 20 * sm.bias
+    scene.fog.far = 1 + 40 * sm.bias;
 };
 
