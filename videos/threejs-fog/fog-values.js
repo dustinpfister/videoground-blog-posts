6804892f8908af09bcce51b96cf5ab2bbca1a18c
dustinpfister <dustin.pfister@gmail.com>
2022-03-07 15:26:05 -0500

    fog-values video

---
+++ videos/threejs-fog/fog-values.js
@@ -0,0 +1,29 @@
+// init method for the video
+VIDEO.init = function(sm, scene, camera){
+    var fogColor = new THREE.Color('white');
+    scene.background = fogColor;
+    scene.fog = new THREE.Fog(fogColor, 0.25, 15);
+    // CAMERA
+    camera.position.set(0, 3, 8);
+    camera.lookAt(0, 0, 0);
+    // GRID HELPER
+    scene.add(new THREE.GridHelper(8, 8));
+    // MESH
+    let mesh = scene.userData.mesh = new THREE.Mesh(
+        new THREE.SphereGeometry(1, 30, 30),
+        new THREE.MeshStandardMaterial({
+            color: new THREE.Color('red')
+        }));
+    scene.add(mesh);
+    // LIGHT
+    let light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(8, 2, -1).normalize();
+    scene.add(light);
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    let mesh = scene.userData.mesh;
+    mesh.position.z = -8 + 11 * sm.bias;
+};
+
---
+++ videos/threejs-fog/todo.md
@@ -0,0 +1,8 @@
+## threejs-fog video todo list
+
+## () - fog-values video
+* start a fog-values
+* have an example that will chnage what scene.fog is over time
+
+## ( done 03/07/2022 ) - Basic video
+* (done) just have a basic example video of the three.fog constructor
