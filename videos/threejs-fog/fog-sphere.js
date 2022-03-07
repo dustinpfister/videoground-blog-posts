540e48f99cc65cd9e0edbe4374cdbaf2631d92bc
dustinpfister <dustin.pfister@gmail.com>
2022-03-07 15:15:34 -0500

    fog-sphere.js

--- videos/threejs-fog/fog-sphere.js
+++ videos/threejs-fog/fog-sphere.js
@@ -2,22 +2,29 @@
 VIDEO.init = function(sm, scene, camera){
     var fogColor = new THREE.Color('white');
     scene.background = fogColor;
-    scene.fog = new THREE.Fog(fogColor);
-    // static camera
-    camera.position.set(0, 8, 8);
+    scene.fog = new THREE.Fog(fogColor, 0.25, 15);
+    // CAMERA
+    camera.position.set(0, 3, 8);
     camera.lookAt(0, 0, 0);
     // GRID HELPER
     scene.add(new THREE.GridHelper(8, 8));
     // MESH
     let mesh = scene.userData.mesh = new THREE.Mesh(
-        new THREE.SphereGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
+        new THREE.SphereGeometry(1, 30, 30),
+        new THREE.MeshStandardMaterial({
+            color: new THREE.Color('red')
+        }));
     scene.add(mesh);
+    // LIGHT
+    let light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(8, 2, -1).normalize();
+    scene.add(light);
+
 };
 
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
     let mesh = scene.userData.mesh;
-    mesh.position.x = -4 + 8 * sm.bias;
+    mesh.position.z = -8 + 11 * sm.bias;
 };
 
