5c9f0184baf167aea5f777bdea15175560fa4e5a
dustinpfister <dustin.pfister@gmail.com>
2022-03-22 14:34:48 -0400

    using sequences again for video1 of threejs-canvas-texture

--- videos/threejs-canvas-texture/videos/video1.js
+++ videos/threejs-canvas-texture/videos/video1.js
@@ -8,19 +8,73 @@ VIDEO.scripts = [
 // init method for the video
 VIDEO.init = function(sm, scene, camera){
 
+    // TEXT CUBE
+    var textCube = scene.userData.textCube = CanvasTextCube.create({
+        width: 128,
+        height: 128,
+        lineWidth: 7,
+        lineColor: 'rgba(0,100,128,0.8)',
+        lineCount: 9,
+        lines: [
+            ['Fog', 64, 20, 22, 'white'],
+            ['in Three.js.', 64, 45, 22, 'white'],
+            ['( r135 03/11/2022 )', 64, 70, 12, 'gray'],
+            ['video1', 64, 100, 10, 'gray']
+        ]
+    });
+    scene.add(textCube);
+
     // SPHERE MESH
     let sphere = scene.userData.sphere = new THREE.Mesh(
         new THREE.SphereGeometry(1, 30, 30),
         new THREE.MeshStandardMaterial({
             emissive: new THREE.Color('red')
         }));
-    sphere.position.y = 1.5;
     scene.add(sphere);
+
+// SET UP SEQ OBJECT
+    sm.seq = Sequences.create({
+        sm: sm,
+        part : [
+            {
+                per: 0,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    textCube.position.set(6, 2.4, 0);
+                }
+            },
+            {
+                per: 0.1,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+
+                    textCube.position.set(6, 2.4 + 5 * partPer, 0);
+                    textCube.rotation.y = Math.PI * 2 * partPer;
+                }
+            },
+            {
+                per: 0.25,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+
+                }
+            }
+        ]
+    });
 
 };
 
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
+    var textCube = scene.userData.textCube;
 
+    textCube.rotation.y = 0;
+    textCube.position.set(6, 2.4 + 5, 0);
+
+    // sequences
+    Sequences.update(sm.seq, sm);
+
+    // have camera always look at center
+    camera.lookAt(0, 0, 0);
 };
 
