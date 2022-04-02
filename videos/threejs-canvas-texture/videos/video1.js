0f2b191a1df951f1c83ab28cfc406ab87081f893
dustinpfister <dustin.pfister@gmail.com>
2022-04-02 13:29:01 -0400

    custom draw methods object started for video1

--- videos/threejs-canvas-texture/videos/video1.js
+++ videos/threejs-canvas-texture/videos/video1.js
@@ -1,15 +1,15 @@
-// 
+// scripts
 VIDEO.scripts = [
    '../../../js/canvas.js',
    '../../../js/canvas-text-cube.js',
    '../../../js/sequences.js'
 ];
-
+ 
 // init method for the video
 VIDEO.init = function(sm, scene, camera){
-
+ 
     scene.add( new THREE.GridHelper(10, 10))
-
+ 
     // TEXT CUBE
     var textCube = scene.userData.textCube = CanvasTextCube.create({
         width: 128,
@@ -25,16 +25,34 @@ VIDEO.init = function(sm, scene, camera){
         ]
     });
     scene.add(textCube);
+ 
+    var drawMethods = {};
 
+    drawMethods.basic = {};
+
+    drawMethods.basic.square = function(ctx, canvas, sm, opt){
+        ctx.fillStyle = opt.bgStyle || 'gray';
+        ctx.fillRect(-1, -1, canvas.width + 2, canvas.height + 2);
+    };
+ 
+    // canvas mod Object
+    var canvasObj = CanvasMod.createCanvasObject(sm, drawMethods, {
+        width: 32,
+        height: 32
+    });
+
+    canvasObj.draw({drawClass: 'basic', drawMethod: 'square'});
+ 
     // SPHERE MESH
     let sphere = scene.userData.sphere = new THREE.Mesh(
         new THREE.SphereGeometry(1, 30, 30),
         new THREE.MeshStandardMaterial({
-            emissive: new THREE.Color('red')
+            emissive: new THREE.Color('white'),
+            emissiveMap: canvasObj.texture
         }));
     scene.add(sphere);
 
-// SET UP SEQ OBJECT
+    // SET UP SEQ OBJECT
     sm.seq = Sequences.create({
         sm: sm,
         part : [
@@ -51,7 +69,7 @@ VIDEO.init = function(sm, scene, camera){
                 init: function(sm){},
                 update: function(sm, scene, camera, partPer, partBias){
                     textCube.visible = true;
-                    textCube.position.set(6, 1 * partPer, 0);
+                    textCube.position.set(6, 2 * partPer, 0);
                     textCube.rotation.y = Math.PI * 2 * partPer;
                 }
             },
@@ -70,16 +88,12 @@ VIDEO.init = function(sm, scene, camera){
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
     var textCube = scene.userData.textCube;
-
     camera.position.set(8, 0, 0);
-
     textCube.rotation.y = 0;
     textCube.position.set(6, 0, 0);
     textCube.visible = false;
-
     // sequences
     Sequences.update(sm.seq, sm);
-
     // have camera always look at center
     camera.lookAt(0, 0, 0);
 };
