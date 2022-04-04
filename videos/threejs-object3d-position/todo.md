04872750c11fc66e169b219f93a43bbae61e0dc9
dustinpfister <dustin.pfister@gmail.com>
2022-04-04 12:56:06 -0400

    started a video folder for threejs-object3d-position

---
+++ videos/threejs-object3d-position/todo.md
@@ -0,0 +1,5 @@
+## threejs-object3d-position video todo list
+
+
+## ( ) - video1 - start video 1
+* start video1 with a basic sequence that is the movement of a single mesh object
---
+++ videos/threejs-object3d-position/videos/video1.js
@@ -0,0 +1,96 @@
+// scripts
+VIDEO.scripts = [
+   '../../../js/canvas-text-cube.js',
+   '../../../js/sequences.js'
+];
+ 
+// init method for the video
+VIDEO.init = function(sm, scene, camera){
+ 
+    scene.background = new THREE.Color('#202020');
+    scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0xffffff));
+ 
+    // TEXT CUBE
+    var textCube = scene.userData.textCube = CanvasTextCube.create({
+        width: 128,
+        height: 128,
+        lineWidth: 7,
+        lineColor: 'rgba(0,100,128,0.8)',
+        lineCount: 9,
+        lines: [
+            ['Canvas Textures', 64, 20, 16, 'white'],
+            ['in Three.js.', 64, 45, 16, 'white'],
+            ['( r135 04/02/2022 )', 64, 70, 12, 'gray'],
+            ['video1', 64, 100, 10, 'gray']
+        ]
+    });
+    scene.add(textCube);
+ 
+    let cube1 = scene.userData.cube1 = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+    scene.add(cube1);
+ 
+    // SET UP SEQ OBJECT
+    sm.seq = Sequences.create({
+        sm: sm,
+        part : [
+            {
+                per: 0,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // text cube
+                    textCube.visible = true;
+                    textCube.position.set(6, 0.8, 0);
+                    // camera
+                    camera.position.set(8, 1, 0);
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            {
+                per: 0.10,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // move up text cube
+                    textCube.visible = true;
+                    textCube.position.set(6, 1 + 2 * partPer, 0);
+                    textCube.rotation.y = Math.PI * 2 * partPer;
+                    // camera
+                    camera.position.set(8, 1, 0);
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            {
+                per: 0.20,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // camera
+                    camera.position.set(8,1,5 * partPer);
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            {
+                per: 0.40,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // camera
+                    camera.position.set(8 - 16 * partPer,1,5);
+                    camera.lookAt(0, 0, 0);
+                }
+            }
+        ]
+    });
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    var textCube = scene.userData.textCube;
+    textCube.rotation.y = 0;
+    textCube.position.set(6, 0, 0);
+    textCube.visible = false;
+ 
+    // cube1
+    // sequences
+    Sequences.update(sm.seq, sm);
+};
+
