ceab7a414bc55d08a5f5dd612a0fea28d66f8de9
dustinpfister <dustin.pfister@gmail.com>
2022-04-04 17:15:33 -0400

    added mod to video1

--- videos/threejs-grouping-mesh-objects/videos/video1.js
+++ videos/threejs-grouping-mesh-objects/videos/video1.js
@@ -1,9 +1,65 @@
-	// scripts
+// scripts
 VIDEO.scripts = [
    '../../../js/canvas.js',
    '../../../js/canvas-text-cube.js',
    '../../../js/sequences.js'
 ];
+
+
+var Model = (function () {
+    // the constructor
+    var Mod = function (opt) {
+        // this is what will be added to the Scene
+        this.group = new THREE.Group;
+        // set default, or use what is given
+        opt = opt || {};
+        this.radius = opt.radius === undefined ? 4 : opt.radius;
+        this.count = opt.count === undefined ? 5 : opt.count;
+        this.bxSize = opt.bxSize === undefined ? 1 : opt.bxSize;
+        this.color = opt.color === undefined ? 0x00ff00 : opt.color;
+        var i = 0,
+        bx,
+        radian;
+        while (i < this.count) {
+            bx = new THREE.Mesh(
+                    new THREE.BoxGeometry(this.bxSize, this.bxSize, this.bxSize),
+                    new THREE.MeshStandardMaterial({
+                        color: this.color,
+                        emissive: 0x0f0f0f
+ 
+                    }));
+            this.group.add(bx);
+            i += 1;
+        }
+        this.update();
+        console.log(this.group);
+    };
+    // update the group
+    Mod.prototype.update = function () {
+        var i = 0,
+        bx,
+        radian;
+        while (i < this.count) {
+            bx = this.group.children[i];
+            radian = Math.PI * 2 / this.count * i;
+            bx.position.set(
+ 
+                Math.cos(radian) * this.radius,
+                0,
+                Math.sin(radian) * this.radius);
+            bx.lookAt(0, 0, 0);
+            i += 1;
+        };
+    };
+    // set radius and update
+    Mod.prototype.setRadius = function (radius) {
+        this.radius = radius;
+        this.update();
+    };
+    // return the constructor
+    return Mod;
+}
+    ());
  
 // init method for the video
 VIDEO.init = function(sm, scene, camera){
@@ -29,6 +85,21 @@ VIDEO.init = function(sm, scene, camera){
     textCube.material.opacity = 0.8;
     scene.add(textCube);
  
+    // three instances of the model
+    var mod1 = new Model({
+            count: 8,
+            bxSize: 1,
+            color: 0xff0000
+ 
+        });
+    scene.add(mod1.group);
+
+    // light
+    scene.add(new THREE.PointLight().add(new THREE.Mesh(
+                new THREE.SphereGeometry(.5, 10, 10),
+                new THREE.MeshBasicMaterial({
+                    color: 0xffffff
+                }))));
  
     // SET UP SEQ OBJECT
     sm.seq = Sequences.create({
