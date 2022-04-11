cfc5579bf56ba3a94b15da4b51bea66eb7f254c8
dustinpfister <dustin.pfister@gmail.com>
2022-04-11 09:11:56 -0400

    clean start with video1

--- videos/threejs-object3d-scale/videos/video1.js
+++ videos/threejs-object3d-scale/videos/video1.js
@@ -12,10 +12,6 @@ VIDEO.init = function(sm, scene, camera){
     scene.background = new THREE.Color('#2a2a2a');
     scene.add( new THREE.GridHelper(10, 10, '#ffffff', '#00afaf') );
  
-    // LIGHT
-    var dl = new THREE.DirectionalLight(0xffffff, 1);
-    dl.position.set(1, 1, 1);
-    scene.add(dl);
 
     // TEXT CUBE
     var textCube = scene.userData.textCube = CanvasTextCube.create({
@@ -25,65 +21,14 @@ VIDEO.init = function(sm, scene, camera){
         lineColor: 'rgba(0,100,128,0.8)',
         lineCount: 9,
         lines: [
-            ['Object3D.lookAt', 64, 20, 16, 'white'],
+            ['Object3D.scale', 64, 20, 16, 'white'],
             ['in Three.js.', 64, 45, 16, 'white'],
-            ['( r135 04/07/2022 )', 64, 70, 12, 'gray'],
+            ['( r135 04/11/2022 )', 64, 70, 12, 'gray'],
             ['video1', 64, 100, 10, 'gray']
         ]
     });
     scene.add(textCube);
  
-    // DEMO GROUP
-    var demoGroup = new THREE.Group();
-    scene.add(demoGroup);
-    var sphere = new THREE.Mesh( 
-        new THREE.SphereGeometry(0.25, 30, 30), 
-        new THREE.MeshStandardMaterial({
-            color: new THREE.Color('blue'),
-            transparent: true,
-            opacity: 0.5
-        })
-    );
-    demoGroup.add(sphere);
-
-    // CONE GROUP
-    var coneGroup = new THREE.Group();
-    demoGroup.add(coneGroup);
-    var coneMaterial = new THREE.MeshStandardMaterial({
-        color: new THREE.Color('cyan'),
-        transparent: true,
-        opacity: 0.5
-    });
-    // [ [[x, y, z], coneLength], ... ]
-    var coneDataArray = [],
-    len = 8,
-    i = 0, x, y, z, radian, radius = 3;
-    while(i < len){
-        radian = Math.PI * 2 / len * i;
-        x = Math.cos(radian) * radius;
-        y = 0;
-        z = Math.sin(radian) * radius;
-        coneDataArray.push([[ x, y, z], 2]);
-        i += 1;
-    }
-    coneDataArray.forEach(function(coneData){
-        var cone = new THREE.Mesh( new THREE.ConeGeometry(0.5, coneData[1], 30, 30), coneMaterial);
-        cone.geometry.rotateX(1.57);
-        cone.position.fromArray(coneData[0]);
-        cone.position.y += coneData[1] / 2 - 0.8;
-        coneGroup.add(cone);
-    });
-
-    // ALL LOOK HELPER
-    var allLook = function(group, target){
-        var v = new THREE.Vector3();
-        target.getWorldPosition(v);
-        group.children.forEach(function(child){
-            child.lookAt(v);
-        });
-    };
-    allLook(coneGroup, sphere);
-  
 
     // SET UP SEQ OBJECT
     sm.seq = Sequences.create({
@@ -99,11 +44,6 @@ VIDEO.init = function(sm, scene, camera){
                     // camera
                     camera.position.set(8, 1, 0);
                     camera.lookAt(0, 0, 0);
-                    // sphere and cones
-                    sphere.position.y = 0;
-                    allLook(coneGroup, sphere);
-                    // demo group
-                    demoGroup.rotation.set(0, Math.PI / 180 * 22.5, 0);
                 }
             },
             {
@@ -117,47 +57,13 @@ VIDEO.init = function(sm, scene, camera){
                     // camera
                     camera.position.set(8, 1, 0);
                     camera.lookAt(0, 0, 0);
-                    // sphere and cones
-                    sphere.position.y = 0;
-                    allLook(coneGroup, sphere);
-                    // demo group
-                    demoGroup.rotation.set(0, Math.PI / 180 * 22.5, 0);
                 }
             },
             {
                 per: 0.20,
                 init: function(sm){},
                 update: function(sm, scene, camera, partPer, partBias){
-                    // sphere and cones
-                    sphere.position.y = 2 * partPer;
-                    allLook(coneGroup, sphere);
-                    // demo group
-                    demoGroup.rotation.set(0, Math.PI / 180 * 22.5, 0);
-                }
-            },
-            {
-                per: 0.30,
-                init: function(sm){},
-                update: function(sm, scene, camera, partPer, partBias){
-                    // sphere and cones
-                    sphere.position.y = 2 - 4 * partPer;
-                    allLook(coneGroup, sphere);
-                    // demo group
-                    demoGroup.rotation.set(0, Math.PI / 180 * 22.5, 0);
-                }
-            },
-            {
-                per: 0.50,
-                init: function(sm){},
-                update: function(sm, scene, camera, partPer, partBias){
-                    // sphere and cones
-                    sphere.position.y = -2 + 16 * partPer;
-                    allLook(coneGroup, sphere);
-                    // demo group
-                    demoGroup.rotation.set(
-                        Math.PI / 180 * 45 * partPer, 
-                        Math.PI / 180 * 22.5,
-                        Math.PI / 180 * 90 * partPer);
+
                 }
             }
         ]
