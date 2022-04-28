cec98615286443639dbd67b485f9da48b9225269
dustinpfister <dustin.pfister@gmail.com>
2022-04-28 14:26:47 -0400

    video1 looking good so far for waves example

--- videos/threejs-examples-waves/videos/video1.js
+++ videos/threejs-examples-waves/videos/video1.js
@@ -6,6 +6,83 @@ VIDEO.scripts = [
    '../../../js/sequences.js'
 ];
 
+
+    // Wave grid helper
+    var waveGrid = function (opt) {
+        opt = opt || {};
+        opt.width = opt.width || 30;
+        opt.depth = opt.depth || 30;
+        opt.height = opt.height || 2;
+        opt.forPoint = opt.forPoint || function () {};
+        opt.context = opt.context || opt;
+        opt.xStep = opt.xStep || 0.075;
+        opt.yStep = opt.yStep || 0.1;
+        opt.zStep = opt.zStep || 0.075;
+        opt.waveOffset = opt.waveOffset === undefined ? 0 : opt.waveOffset;
+        var points = [],
+        radPer,
+        x = 0,
+        i = 0,
+        y,
+        z;
+        // points
+        while (x < opt.width) {
+            z = 0;
+            while (z < opt.depth) {
+                // radian percent
+                radPer = (z / opt.depth + (1 / opt.width * x) + opt.waveOffset) % 1;
+                // y value of point
+                y = Math.cos(Math.PI * 4 * radPer) * opt.height;
+                // call forPoint
+                opt.forPoint.call(opt.context, x * opt.xStep, y * opt.yStep, z * opt.zStep, i);
+                // step z, and point index
+                z += 1;
+                i += 3;
+            }
+            x += 1;
+        };
+    };
+
+   // make a points mesh
+    var makePoints = function () {
+        var geometry = new THREE.BufferGeometry();
+        var points = [],
+        opt = {};
+        opt.forPoint = function (x, y, z, i) {
+            points.push(x, y, z);
+        };
+        waveGrid(opt);
+        var vertices = new Float32Array(points);
+        // itemSize = 3 because there are 3 values (components) per vertex
+        geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
+        return new THREE.Points(
+            // geometry as first argument
+            geometry,
+            // then Material
+            new THREE.PointsMaterial({
+                size: .125,
+                color: new THREE.Color(0.0, 0.25, 0.25)
+            }));
+    };
+
+
+    // update points
+    var updatePoints = function (points, per) {
+        var position = points.geometry.getAttribute('position');
+        // update points
+        waveGrid({
+            waveOffset: per,
+            xStep: 0.125,
+            zStep: 0.125,
+            forPoint: function (x, y, z, i) {
+                position.array[i] = x - 2;
+                position.array[i + 1] = y - 2;
+                position.array[i + 2] = z - 2;
+            }
+        });
+        position.needsUpdate = true;
+    }
+
 // init method for the video
 VIDEO.init = function(sm, scene, camera){
  
@@ -32,6 +109,9 @@ VIDEO.init = function(sm, scene, camera){
         ]
     });
     scene.add(textCube);
+
+    var points = scene.userData.points = makePoints();
+    scene.add(points);
 
     // SET UP SEQ OBJECT
     sm.seq = Sequences.create({
@@ -105,6 +185,9 @@ VIDEO.update = function(sm, scene, camera, per, bias){
     textCube.rotation.y = 0;
     textCube.position.set(8, 0.8, 0);
     textCube.visible = false;
+
+    var points = scene.userData.points;
+    updatePoints(points, per * 8 % 1);
 
     // sequences
     Sequences.update(sm.seq, sm);
