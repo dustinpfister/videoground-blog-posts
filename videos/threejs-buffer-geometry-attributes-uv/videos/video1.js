9a291106d11a87cbdf64ffb774c801c86c58b7ea
dustinpfister <dustin.pfister@gmail.com>
2022-04-30 09:41:03 -0400

    new cam pos

--- videos/threejs-buffer-geometry-attributes-uv/videos/video1.js
+++ videos/threejs-buffer-geometry-attributes-uv/videos/video1.js
@@ -33,6 +33,63 @@ VIDEO.init = function(sm, scene, camera){
     scene.add(textCube);
 
 
+    // creating a simple canvas generated texture
+    var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+    canvas.width = 32;
+    canvas.height = 32;
+    ctx.fillStyle = '#004040'; // fill
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'red';
+    ctx.beginPath(); // draw red and white circle
+    ctx.arc(10, 10, 8, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
+    ctx.beginPath(); // draw white square
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.stroke();
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+ 
+    // GEOMETRY - starting with a plane
+    var geometry = new THREE.PlaneGeometry(4, 4, 1, 1);
+ 
+    var uv = geometry.getAttribute('uv'),
+    position = geometry.getAttribute('position');
+ 
+    // MUTATING THE UV VALUES
+    uv.array[0] = 0.27;
+    uv.array[1] = 0.73;
+ 
+    uv.array[2] = 0.73;
+    uv.array[3] = 0.73;
+ 
+    uv.array[4] = 0.27;
+    uv.array[5] = 0.27;
+ 
+    uv.array[6] = 0.73;
+    uv.array[7] = 0.27;
+ 
+    // use the geometry with a mesh
+    var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+                side: THREE.DoubleSide,
+                map: texture
+            }));
+    mesh.position.set(0, 0, 2.5);
+    mesh.rotation.set(0, Math.PI * 0.5, 0);
+    scene.add(mesh);
+ 
+    // another mesh where I am not doing anything to the uv values
+    var geometry = new THREE.PlaneGeometry(4, 4, 1, 1);
+    var mesh2 = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+                side: THREE.DoubleSide,
+                map: texture
+            }));
+    mesh2.position.set(0, 0, -2.5);
+    mesh2.rotation.set(0, Math.PI * 0.5, 0);
+    scene.add(mesh2);
+
     // SET UP SEQ OBJECT
     sm.seq = Sequences.create({
         sm: sm,
@@ -55,7 +112,7 @@ VIDEO.init = function(sm, scene, camera){
                 update: function(sm, scene, camera, partPer, partBias){
                     // move up text cube
                     textCube.visible = true;
-                    textCube.position.set(6, 1 + 2 * partPer, 0);
+                    textCube.position.set(6, 0.8 + 2 * partPer, 0);
                     textCube.rotation.y = Math.PI * 2 * partPer;
                     // camera
                     camera.position.set(8, 1, 0);
@@ -68,7 +125,7 @@ VIDEO.init = function(sm, scene, camera){
                 init: function(sm){},
                 update: function(sm, scene, camera, partPer, partBias){
                     // camera
-                    camera.position.set(8 + 2 * partPer, 1 + 5 * partPer, 10 * partPer);
+                    camera.position.set(8, 1, 0);
                     camera.lookAt(0, 0, 0);
                 }
             }
