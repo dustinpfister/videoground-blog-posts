6942df0d23aef73061ff6c9fbee10fb81a7ea277
dustinpfister <dustin.pfister@gmail.com>
2022-04-30 09:28:27 -0400

    templates folder

---
+++ templates/template1/README.md
@@ -0,0 +1,3 @@
+# template1 video 
+
+This is a clean template video project that will work as an okay starting point for most blog post videos. Just make a copy of his folder to the videos folder making the needed chnages to the folder name, this readme file, and video1.js
---
+++ templates/template1/videos/video1.js
@@ -0,0 +1,90 @@
+// video1 for threejs-geometry-attributes-uv
+// scripts
+VIDEO.scripts = [
+   '../../../js/canvas.js',
+   '../../../js/canvas-text-cube.js',
+   '../../../js/sequences.js'
+];
+
+
+// init method for the video
+VIDEO.init = function(sm, scene, camera){
+ 
+    // BACKGROUND
+    scene.background = new THREE.Color('#2a2a2a');
+    var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    scene.add( grid );
+ 
+    // TEXT CUBE
+    var textCube = scene.userData.textCube = CanvasTextCube.create({
+        width: 128,
+        height: 128,
+        lineWidth: 7,
+        lineColor: 'rgba(0,100,128,0.8)',
+        lineCount: 9,
+        lines: [
+            ['UV Attribute of', 64, 17, 14, 'white'],
+            ['Buffer Geometry', 64, 32, 14, 'white'],
+            ['in Three.js.', 64, 47, 14, 'white'],
+            ['( r135 04/30/2022 )', 64, 70, 12, 'gray'],
+            ['video1', 64, 100, 10, 'gray']
+        ]
+    });
+    scene.add(textCube);
+
+
+    // SET UP SEQ OBJECT
+    sm.seq = Sequences.create({
+        sm: sm,
+        part : [
+            {
+                per: 0,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // text cube
+                    textCube.visible = true;
+                    textCube.position.set(6, 0.8, 0);
+                    // camera
+                    camera.position.set(8, 1, 0);
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            {
+                per: 0.10,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // move up text cube
+                    textCube.visible = true;
+                    textCube.position.set(6, 1 + 2 * partPer, 0);
+                    textCube.rotation.y = Math.PI * 2 * partPer;
+                    // camera
+                    camera.position.set(8, 1, 0);
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            // sq1 - 
+            {
+                per: 0.15,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // camera
+                    camera.position.set(8 + 2 * partPer, 1 + 5 * partPer, 10 * partPer);
+                    camera.lookAt(0, 0, 0);
+                }
+            }
+        ]
+    });
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    var textCube = scene.userData.textCube;
+    textCube.rotation.y = 0;
+    textCube.position.set(8, 1, 0);
+    textCube.visible = false;
+
+    // sequences
+    Sequences.update(sm.seq, sm);
+
+};
+
