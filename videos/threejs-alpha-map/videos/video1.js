97ecdb1044a62e05167a146b596d039768209f73
dustinpfister <dustin.pfister@gmail.com>
2022-05-01 09:19:38 -0400

    started with the example from the post

--- videos/threejs-alpha-map/videos/video1.js
+++ videos/threejs-alpha-map/videos/video1.js
@@ -30,6 +30,41 @@ VIDEO.init = function(sm, scene, camera){
     });
     scene.add(textCube);
 
+ 
+// creating a texture with canvas
+var canvas = document.createElement('canvas'),
+ctx = canvas.getContext('2d');
+canvas.width = 64;
+canvas.height = 64;
+// drawing gray scale areas
+ctx.fillStyle = '#404040';
+ctx.fillRect(0, 0, 32, 32);
+ctx.fillStyle = '#808080';
+ctx.fillRect(32, 0, 32, 32);
+ctx.fillStyle = '#c0c0c0';
+ctx.fillRect(0, 32, 32, 32);
+ctx.fillStyle = '#f0f0f0';
+ctx.fillRect(32, 32, 32, 32);
+var texture = new THREE.CanvasTexture(canvas);
+ 
+// creating a mesh that is using the Basic material
+var mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({
+            color: 0x00ffff,
+            // using the alpha map property to set the texture
+            // as the alpha map for the material
+            alphaMap: texture,
+            // I also need to make sure the transparent
+            // property is true
+            transparent: true,
+            // even when opacity is one the alpha map will 
+            // still effect transparency this can just be used to set it even lower
+            opacity: 1,
+            side: THREE.DoubleSide
+        }));
+scene.add(mesh);
+
 
     // SET UP SEQ OBJECT
     sm.seq = Sequences.create({
