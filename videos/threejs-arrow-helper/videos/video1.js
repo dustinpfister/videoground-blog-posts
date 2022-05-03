5230d5ea891404fde4e78cb4131a914a561b2486
dustinpfister <dustin.pfister@gmail.com>
2022-05-03 08:56:52 -0400

    adjust position of grid

--- videos/threejs-arrow-helper/videos/video1.js
+++ videos/threejs-arrow-helper/videos/video1.js
@@ -14,6 +14,7 @@ VIDEO.init = function(sm, scene, camera){
 
     // GRID
     var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#ffffff');
+    grid.position.y = -0.001;
     scene.add( grid );
  
     // TEXT CUBE
