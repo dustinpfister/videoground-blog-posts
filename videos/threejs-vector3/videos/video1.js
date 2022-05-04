8af555709d5e18dbd81cfac30be80c3a960fd750
dustinpfister <dustin.pfister@gmail.com>
2022-05-04 12:46:08 -0400

    started a create points method for post on vector3

--- videos/threejs-vector3/videos/video1.js
+++ videos/threejs-vector3/videos/video1.js
@@ -25,13 +25,67 @@ VIDEO.init = function(sm, scene, camera){
         lineCount: 9,
         lines: [
             ['The Vector3', 64, 17, 14, 'white'],
-            ['class in', 64, 32, 14, 'white'],
+            ['Class in', 64, 32, 14, 'white'],
             ['three.js', 64, 47, 14, 'white'],
-            ['( r135 05/03/2022 )', 64, 70, 12, 'gray'],
+            ['( r135 05/04/2022 )', 64, 70, 12, 'gray'],
             ['video1', 64, 100, 10, 'gray']
         ]
     });
     scene.add(textCube);
+
+
+    // create points helper
+    var createPoints = function(len, radius){
+        radius = radius === undefined ? 5 : radius;
+        var points = [];
+        var i = 0, v, radian, per;
+        while(i < len){
+            per = i / ( len - 1 );
+            radian = Math.PI * 2 * per
+            v = new THREE.Vector3();
+            v.x = Math.cos(radian) * radius;
+            v.z = Math.sin(radian) * radius;
+            v.y = 0;
+            points.push(v);
+            i += 1;
+        };
+        return points;
+    };
+
+
+    var updateLinesGroup = function(lines, r){
+        lines.children.forEach(function(line, i, arr){
+            var per = (i + 1) / arr.length,
+            bias = 1 - Math.abs(0.5 - per) / 0.5;
+
+            line.geometry.setFromPoints( createPoints(150, bias * r) );
+            line.position.y = r * -1 + r * 2 * per;
+        });
+    };
+
+
+
+    // create lines group
+    var lines = new THREE.Group();
+    var lineCount = 12;
+    var colors = [0x00ff00, 0xff0000, 0x0000ff, 0xff00ff, 0x00ffff, 0xffff00];
+    var i = 0;
+    while(i < lineCount){
+        //var per = i / lineCount,
+        //bias = 1 - Math.abs(0.5 - per) / 0.5;
+        var points = createPoints(150, 2 );
+        var geometry = new THREE.BufferGeometry().setFromPoints( points );
+        var line = scene.userData.line = new THREE.Line(
+            geometry,
+            new THREE.LineBasicMaterial({
+                color: colors[i % colors.length],
+                linewidth: 6
+            }));
+        lines.add(line);
+        i += 1;
+    }
+    scene.add(lines);
+    updateLinesGroup(lines, 2);
 
 
     // SET UP SEQ OBJECT
