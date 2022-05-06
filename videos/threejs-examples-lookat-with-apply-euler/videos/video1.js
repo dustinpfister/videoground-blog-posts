fa1c5d63d6d6716484ea804af1880102b713fbbb
dustinpfister <dustin.pfister@gmail.com>
2022-05-06 13:26:42 -0400

    radius argument

--- videos/threejs-examples-lookat-with-apply-euler/videos/video1.js
+++ videos/threejs-examples-lookat-with-apply-euler/videos/video1.js
@@ -33,6 +33,97 @@ VIDEO.init = function(sm, scene, camera){
     });
     scene.add(textCube);
 
+//******** **********
+// LIGHT
+//******** **********
+var dl = new THREE.DirectionalLight(0xffffff, 1);
+dl.position.set(1,3,2);
+scene.add(dl);
+
+//******** **********
+// HELPER FUNCTIONS
+//******** **********
+// materuials to use for mesh objects
+var materials = [
+    new THREE.MeshStandardMaterial({color: new THREE.Color('cyan')}),
+    new THREE.MeshStandardMaterial({color: new THREE.Color('red')})
+];
+
+// make a part of the object
+var mkPart = function(g, partName, w, h, d, x, y, z, mi){
+    // the mesh object
+    var m = new THREE.Mesh(
+    new THREE.BoxGeometry(w, h, d),
+    materials[mi === undefined ? 0 : mi]);
+    // name of part
+    m.name = g.name + '_' + partName;
+    // position it
+    m.position.set(x, y, z);
+    return m;
+};
+
+// make the whole group with all parts
+var mkModel = function(gName){
+    var g = new THREE.Group();
+    g.name = gName || 'g-' + g.uuid;
+    // add parts
+    g.add( mkPart(g, 'body', 1, 1, 4, 0, 0, 0) );
+    g.add( mkPart(g, 'tail', 0.5, 1, 1, 0, 1, -1.5, 1) );
+    g.add( mkPart(g, 'rwing', 2, 0.5, 1, -1.5, 0, 0) );
+    g.add( mkPart(g, 'lwing', 2, 0.5, 1, 1.5, 0, 0) );
+    return g;
+};
+
+// make a collection of them
+var createWrap = function(){
+    var wrap = new THREE.Group();
+    var i = 0, count = 50;
+    while(i < count){
+        var g = mkModel('g' + i);
+        wrap.add(  g );
+        i += 1;
+    }
+    wrap.scale.set(0.5, 0.5, 0.5);
+    return wrap;
+};
+
+// position a wrap object
+var positionWrap = function(wrap, bias, ringCount, radius){
+
+    bias = bias === undefined ? 1 : bias;
+    ringCount = ringCount === undefined ? 5 : ringCount;
+    radius = radius === undefined ? 5 : radius;
+
+    var count = wrap.children.length,
+    i = 0;
+    perRing = count / ringCount;
+
+    var yaStep = 90 / ringCount;
+    while(i < count){
+        var per = i / count;
+        var g = wrap.children[i];
+        var ring = Math.floor( i / perRing );
+        var rPer = ( i - perRing * ring) / perRing;
+        var x = Math.PI * 2 * rPer, 
+        s = ring < ringCount / 2 ? 0 : 1;
+        y = Math.PI / 180 * yaStep * ring * bias, 
+        z = 0;
+        var e = new THREE.Euler(x, y, z);
+        g.position.set(0, 0, radius).applyEuler( e );
+        g.lookAt(0, 0, 0);
+        i += 1;
+    }
+};
+
+//
+var wrapA = createWrap();
+positionWrap(wrapA, 1);
+scene.add(wrapA);
+
+var wrapB = createWrap();
+positionWrap(wrapB, -1);
+scene.add(wrapB);
+
 
     // SET UP SEQ OBJECT
     sm.seq = Sequences.create({
@@ -49,6 +140,9 @@ VIDEO.init = function(sm, scene, camera){
                     // camera
                     camera.position.set(8, 1, 0);
                     camera.lookAt(0, 0, 0);
+                    // wrap objects
+                    positionWrap(wrapA, 0, 5);
+                    positionWrap(wrapB, 0, 5 );
                 }
             },
             {
@@ -63,6 +157,9 @@ VIDEO.init = function(sm, scene, camera){
                     // camera
                     camera.position.set(8, 1, 0);
                     camera.lookAt(0, 0, 0);
+                    // wrap objects
+                    positionWrap(wrapA, 0, 5);
+                    positionWrap(wrapB, 0, 5 );
                 }
             },
             // sq1 - 
@@ -71,8 +168,11 @@ VIDEO.init = function(sm, scene, camera){
                 init: function(sm){},
                 update: function(sm, scene, camera, partPer, partBias){
                     // camera
-                    camera.position.set(8, 1 + 5 * partPer, 0);
+                    camera.position.set(8 + 4 * partPer, 1 + 11 * partPer, 12 * partPer);
                     camera.lookAt(0, 0, 0);
+                    // wrap objects
+                    positionWrap(wrapA, 0, 5);
+                    positionWrap(wrapB, 0, 5 );
                 }
             }
         ]
