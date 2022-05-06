fe22c48757dfa592f670ec223e577b64a226fdf1
dustinpfister <dustin.pfister@gmail.com>
2022-05-06 13:45:16 -0400

    new video folder started

---
+++ videos/threejs-box-geometry/videos/video1.js
@@ -0,0 +1,93 @@
+// video1 for threejs-box-geometry
+ 
+// scripts
+VIDEO.scripts = [
+   '../../../js/canvas.js',
+   '../../../js/canvas-text-cube.js',
+   '../../../js/sequences.js'
+];
+// init
+VIDEO.init = function(sm, scene, camera){
+ 
+    // BACKGROUND
+    scene.background = new THREE.Color('#2a2a2a');
+
+    // GRID
+    var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    scene.add( grid );
+ 
+    // TEXT CUBE
+    var textCube = scene.userData.textCube = CanvasTextCube.create({
+        width: 128,
+        height: 128,
+        lineWidth: 7,
+        lineColor: 'rgba(0,100,128,0.8)',
+        lineCount: 9,
+        lines: [
+            ['Box Geometry', 64, 17, 14, 'white'],
+            ['Constructor in', 64, 32, 14, 'white'],
+            ['threejs', 64, 47, 14, 'white'],
+            ['( r135 05/06/2022 )', 64, 70, 12, 'gray'],
+            ['video1', 64, 100, 10, 'gray']
+        ]
+    });
+    scene.add(textCube);
+
+
+    // SET UP SEQ OBJECT
+    sm.seq = Sequences.create({
+        sm: sm,
+        part : [
+            {
+                per: 0,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // text cube
+                    textCube.visible = true;
+                    textCube.position.set(6, 0.8, 0);
+                    textCube.material.opacity = 1.0;
+                    // camera
+                    camera.position.set(8, 1, 0);
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            {
+                per: 0.10,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // move up text cube
+                    textCube.visible = true;
+                    textCube.position.set(6, 0.8 + 1 * partPer, 0);
+                    textCube.rotation.y = Math.PI * 2 * partPer;
+                    textCube.material.opacity = 1.0 - partPer;
+                    // camera
+                    camera.position.set(8, 1, 0);
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            // sq1 - 
+            {
+                per: 0.15,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // camera
+                    camera.position.set(8, 1 + 5 * partPer, 0);
+                    camera.lookAt(0, 0, 0);
+                }
+            }
+        ]
+    });
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    var textCube = scene.userData.textCube;
+    textCube.rotation.y = 0;
+    textCube.position.set(6, 0.8, 0);
+    textCube.visible = false;
+    textCube.material.transparent = true;
+    textCube.material.opacity = 0.0;
+    // sequences
+    Sequences.update(sm.seq, sm);
+};
+
