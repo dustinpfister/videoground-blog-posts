0e106d47af825af88f0df82c3b6f7859f835b20e
dustinpfister <dustin.pfister@gmail.com>
2022-05-11 09:07:01 -0400

    first seq of grid helper video looks good

--- videos/threejs-grid-helper/videos/video1.js
+++ videos/threejs-grid-helper/videos/video1.js
@@ -11,11 +11,6 @@ VIDEO.init = function(sm, scene, camera){
  
     // BACKGROUND
     scene.background = new THREE.Color('#2a2a2a');
-
-    // GRID
-    var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
-    grid.material.linewidth = 3;
-    scene.add( grid );
  
     // TEXT CUBE
     var textCube = scene.userData.textCube = CanvasTextCube.create({
@@ -34,6 +29,20 @@ VIDEO.init = function(sm, scene, camera){
     });
     scene.add(textCube);
 
+    // GRID HELPERS
+    var i = 0, len = 3,
+    helpers = new THREE.Group();
+    while(i < len){
+        var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#8f8f8f', '#ffffff');
+        grid.material.linewidth = 3;
+        helpers.add( grid );
+        i += 1;
+    }
+    scene.add(helpers);
+
+    var helpersDefault = function(helper){
+        helper.scale.set(0, 0, 0);
+    };
 
     // SET UP SEQ OBJECT
     sm.seq = Sequences.create({
@@ -47,6 +56,10 @@ VIDEO.init = function(sm, scene, camera){
                     textCube.visible = true;
                     textCube.position.set(6, 0.8, 0);
                     textCube.material.opacity = 1.0;
+                    // helpers
+                    helpers.children.forEach(function(helper){
+                        helpersDefault(helper);
+                    });
                     // camera
                     camera.position.set(8, 1, 0);
                     camera.lookAt(0, 0, 0);
@@ -61,6 +74,10 @@ VIDEO.init = function(sm, scene, camera){
                     textCube.position.set(6, 0.8 + 1 * partPer, 0);
                     textCube.rotation.y = Math.PI * 2 * partPer;
                     textCube.material.opacity = 1.0 - partPer;
+                    // helpers
+                    helpers.children.forEach(function(helper){
+                        helpersDefault(helper);
+                    });
                     // camera
                     camera.position.set(8, 1, 0);
                     camera.lookAt(0, 0, 0);
@@ -71,6 +88,12 @@ VIDEO.init = function(sm, scene, camera){
                 per: 0.15,
                 init: function(sm){},
                 update: function(sm, scene, camera, partPer, partBias){
+                    // helpers
+                    helpers.children.forEach(function(helper){
+                        helpersDefault(helper);
+                    });
+var s = partPer;
+helpers.children[0].scale.set(s, s, s);
                     // camera
                     camera.position.set(8, 1 + 5 * partPer, 0);
                     camera.lookAt(0, 0, 0);
