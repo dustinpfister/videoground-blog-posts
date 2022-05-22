d6b8f7991c59a9e1294c65f265efbbd71d660937
dustinpfister <dustin.pfister@gmail.com>
2022-05-22 11:58:58 -0400

    better static position of light

--- videos/threejs-buffer-geometry-compute-vertex-normals/videos/video1.js
+++ videos/threejs-buffer-geometry-compute-vertex-normals/videos/video1.js
@@ -35,6 +35,84 @@ VIDEO.init = function(sm, scene, camera){
     });
     scene.add(textCube);
 
+// LIGHT
+var light = new THREE.PointLight(0xffffff, 1);
+light.position.set(0, 5, -7);
+scene.add(light);
+
+//
+// ADJUST PLANE POINT HELPER
+var adjustPlanePoint = function (geo, vertIndex, yAdjust) {
+    // get position and normal
+    var position = geo.getAttribute('position');
+    var normal = geo.getAttribute('normal');
+    var i = vertIndex * 3;
+    // ADJUSTING POSITION ( Y Only for now )
+    position.array[i + 1] = yAdjust;
+    position.needsUpdate = true;
+};
+// update a geo
+var updatePlaneGeo = function(geo, bias, computeNormals){
+    computeNormals = computeNormals || false;
+    adjustPlanePoint(geo, 0, 0 + 0.75 * bias);
+    adjustPlanePoint(geo, 1, 0.75 - 1.00 * bias);
+    adjustPlanePoint(geo, 2, 0.1);
+    adjustPlanePoint(geo, 8, -0.4 * bias);
+    // ADJUSTING NORMALS USING computeVertexNormals method
+    if(computeNormals){
+        geo.computeVertexNormals();
+    }
+};
+// create a data texture
+var createDataTexture = function (pr, pg, pb) {
+    // USING THREE DATA TEXTURE To CREATE A RAW DATA TEXTURE
+    // Using the seeded random method of the MathUtils object
+    var width = 16,
+    height = 16;
+    var size = width * height;
+    var data = new Uint8Array(4 * size);
+    for (let i = 0; i < size; i++) {
+        var stride = i * 4;
+        var v = Math.floor(THREE.MathUtils.seededRandom() * 255);
+        data[stride] = v * pr;
+        data[stride + 1] = v * pg;
+        data[stride + 2] = v * pb;
+        data[stride + 3] = 255;
+    }
+    var texture = new THREE.DataTexture(data, width, height);
+    texture.needsUpdate = true;
+    return texture;
+};
+// MESH
+var geo1 = new THREE.PlaneGeometry(1, 1, 2, 2);
+geo1.rotateX(Math.PI * 1.5);
+var plane1 = new THREE.Mesh(
+        geo1,
+        new THREE.MeshStandardMaterial({
+            color: 0xffffff,
+            map: createDataTexture(0,1,0),
+            side: THREE.DoubleSide
+        }));
+plane1.position.set(0.6, 0.01, 0);
+scene.add(plane1);
+var geo2 = new THREE.PlaneGeometry(1, 1, 2, 2);
+geo2.rotateX(Math.PI * 1.5);
+var plane2 = new THREE.Mesh(
+        geo2,
+        new THREE.MeshStandardMaterial({
+            color: 0xffffff,
+            map: createDataTexture(0,1,1),
+            side: THREE.DoubleSide
+        }));
+plane2.position.set(-0.6, 0.01, 0);
+scene.add(plane2);
+// USING THE THREE.VertexNormalsHelper method
+const helper1 = new THREE.VertexNormalsHelper(plane1, 2, 0x00ff00, 1);
+scene.add(helper1);
+const helper2 = new THREE.VertexNormalsHelper(plane2, 2, 0x00ffff, 1);
+scene.add(helper2);
+
+
     // A SEQ FOR TEXT CUBE
     var seq_textcube = seqHooks.create({
         setPerValues: false,
@@ -74,6 +152,12 @@ VIDEO.init = function(sm, scene, camera){
     var seq = scene.userData.seq = seqHooks.create({
         fps: 30,
         beforeObjects: function(seq){
+
+    updatePlaneGeo(geo1, seq.bias, true);
+    updatePlaneGeo(geo2, seq.bias, false);
+    helper1.update();
+    helper2.update();
+
             textCube.visible = false;
             camera.position.set(8, 1, 0);
         },
@@ -92,18 +176,19 @@ VIDEO.init = function(sm, scene, camera){
                 }
             },
             {
-                secs: 7,
+                secs: 2,
                 update: function(seq, partPer, partBias){
                     // camera
-                    camera.position.set(8, 1 + 7 * partPer, 8 * partPer);
+                    camera.position.set(8 - 7 * partPer, 1, 1 * partPer);
                     camera.lookAt(0, 0, 0);
                 }
             },
             {
-                secs: 20,
+                secs: 25,
                 update: function(seq, partPer, partBias){
                     // camera
-                    camera.position.set(8, 8, 8);
+                    var s = 1 + 1 * partPer;
+                    camera.position.set(s, s, s);
                     camera.lookAt(0, 0, 0);
                 }
             }
