0d2455d9cea5b83409cc28de2fe4e4f75f0f37a5
dustinpfister <dustin.pfister@gmail.com>
2022-05-23 10:43:49 -0400

    better position for the directional light

--- videos/threejs-userdata/videos/video1.js
+++ videos/threejs-userdata/videos/video1.js
@@ -41,8 +41,9 @@ VIDEO.init = function(sm, scene, camera){
 //******** **********
 // LIGHT
 //******** **********
+
 var dl = new THREE.DirectionalLight(0xafafaf, 1);
-dl.position.set(1, 1, 1);
+dl.position.set(1, 3, 2);
 scene.add(dl);
 
 
