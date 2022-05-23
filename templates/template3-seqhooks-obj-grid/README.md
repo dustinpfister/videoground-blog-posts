2513d6989faa8f195eb21708ececc01e8ece7723
dustinpfister <dustin.pfister@gmail.com>
2022-05-23 15:56:23 -0400

    data textures for template3

--- templates/template3-seqhooks-obj-grid/README.md
+++ templates/template3-seqhooks-obj-grid/README.md
@@ -1,1 +1,3 @@
-# template 2 using threejs-examples-sequence-hooks
+# template 2 using threejs-examples-seqhooks-obj-grid
+
+Template using sequence hooks, and object grid
--- templates/template3-seqhooks-obj-grid/videos/video1.js
+++ templates/template3-seqhooks-obj-grid/videos/video1.js
@@ -1,10 +1,12 @@
-// video1 for template2-sequence-hooks
+// video1 for template3-seqhooks-obj-grid
  
 // scripts
 VIDEO.scripts = [
    '../../../js/canvas.js',
    '../../../js/canvas-text-cube.js',
-   '../../../js/sequences-hooks-r1.js'
+   '../../../js/sequences-hooks-r1.js',
+   '../../../js/datatex.js',
+   '../../../js/object-grid-wrap-r1.js'
 ];
 // init
 VIDEO.init = function(sm, scene, camera){
@@ -16,8 +18,10 @@ VIDEO.init = function(sm, scene, camera){
     var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
     grid.material.linewidth = 3;
     scene.add( grid );
- 
+
+    //******** **********
     // TEXT CUBE
+    //******** **********
     var textCube = scene.userData.textCube = CanvasTextCube.create({
         width: 128,
         height: 128,
@@ -25,7 +29,7 @@ VIDEO.init = function(sm, scene, camera){
         lineColor: 'rgba(0,100,128,0.8)',
         lineCount: 9,
         lines: [
-            ['templae2', 64, 17, 14, 'white'],
+            ['templae3', 64, 17, 14, 'white'],
             ['', 64, 32, 14, 'white'],
             ['', 64, 47, 14, 'white'],
             ['( r135 dd/mm/yyyy )', 64, 70, 12, 'gray'],
@@ -33,6 +37,75 @@ VIDEO.init = function(sm, scene, camera){
         ]
     });
     scene.add(textCube);
+
+    //******** **********
+    // LIGHT
+    //******** **********
+    var dl = new THREE.DirectionalLight(0xffffff, 1);
+    dl.position.set(2, 1, 3);
+    scene.add(dl);
+
+    //******** **********
+    // TEXTURES
+    //******** **********
+    var texture_rnd1 = datatex.seededRandom(40, 40, 1, 1, 1, [0, 255]);
+
+    //******** **********
+    // GRID OPTIONS
+    //******** **********
+    var tw = 9,
+    th = 9,
+    space = 1.5;
+    // source objects
+    var mkBox = function(color, h){
+        var box = new THREE.Group();
+        var a = space * 0.95;
+        var mesh = new THREE.Mesh(
+            new THREE.BoxGeometry( a, h, a),
+            new THREE.MeshStandardMaterial({ color: color, map: texture_rnd1 }) );
+        mesh.position.y = h / 2;
+        //mesh.rotation.y = Math.PI / 180 * 20 * -1;
+        var ground = new THREE.Mesh(
+            new THREE.BoxGeometry( space, 0.1, space),
+            new THREE.MeshStandardMaterial({ color: 0xffffff, map: texture_rnd1}) );
+        ground.position.y = 0.05 * -1;
+        box.add(mesh)  
+        box.add(ground);
+        return box;
+    };
+    var array_source_objects = [
+        mkBox(0x00ffff, 0.25), //new THREE.Object3D(),
+        mkBox(0xff0000, 10.00),
+        mkBox(0xffff00, 4.50),
+        mkBox(0x00ff00, 1.50)
+    ];
+
+    var array_oi = [
+        0,0,0,0,0,3,3,0,0,
+        0,0,0,0,3,2,3,0,0,
+        0,0,0,3,2,3,3,0,0,
+        0,0,3,2,2,2,3,0,0,
+        0,3,2,2,1,2,3,0,0,
+        3,2,3,2,2,2,2,3,0,
+        0,3,0,3,3,3,2,3,0,
+        0,0,0,0,0,0,3,3,0,
+        0,0,0,0,0,0,0,0,0
+    ]
+    //******** **********
+    // CREATE GRID
+    //******** **********
+    var grid = ObjectGridWrap.create({
+        space: space,
+        tw: tw,
+        th: th,
+        //aOpacity: 1.25,
+        dAdjust: 1.25,
+        effects: ['opacity', 'scale', 'rotationB'],
+        sourceObjects: array_source_objects,
+        objectIndices: array_oi
+    });
+    scene.add(grid);
+
 
     // A SEQ FOR TEXT CUBE
     var seq_textcube = seqHooks.create({
@@ -73,6 +146,10 @@ VIDEO.init = function(sm, scene, camera){
     var seq = scene.userData.seq = seqHooks.create({
         fps: 30,
         beforeObjects: function(seq){
+
+            ObjectGridWrap.setPos(grid, (1 - seq.per) * 2, Math.cos(Math.PI * seq.bias) * 0.25 );
+            ObjectGridWrap.update(grid);
+
             textCube.visible = false;
             camera.position.set(8, 1, 0);
         },
