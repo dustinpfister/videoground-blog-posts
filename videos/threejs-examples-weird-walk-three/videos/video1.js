4ff89a35d616289c31f5ff255640d0d071333c1e
dustinpfister <dustin.pfister@gmail.com>
2022-05-24 11:57:51 -0400

    have video up and running

--- videos/threejs-examples-weird-walk-three/videos/video1.js
+++ videos/threejs-examples-weird-walk-three/videos/video1.js
@@ -6,7 +6,8 @@ VIDEO.scripts = [
    '../../../js/canvas-text-cube.js',
    '../../../js/sequences-hooks-r1.js',
    '../../../js/datatex.js',
-   '../../../js/object-grid-wrap-r1.js'
+   '../../../js/object-grid-wrap-r1.js',
+   '../js/weird-walk-three.js'
 ];
 // init
 VIDEO.init = function(sm, scene, camera){
@@ -15,9 +16,9 @@ VIDEO.init = function(sm, scene, camera){
     scene.background = new THREE.Color('#2a2a2a');
 
     // GRID
-    var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
-    grid.material.linewidth = 3;
-    scene.add( grid );
+    //var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    //grid.material.linewidth = 3;
+    //scene.add( grid );
 
     //******** **********
     // TEXT CUBE
@@ -50,61 +51,83 @@ VIDEO.init = function(sm, scene, camera){
     //******** **********
     var texture_rnd1 = datatex.seededRandom(40, 40, 1, 1, 1, [0, 255]);
 
-    //******** **********
-    // GRID OPTIONS
-    //******** **********
-    var tw = 9,
-    th = 9,
-    space = 1.5;
-    // source objects
-    var mkBox = function(color, h){
-        var box = new THREE.Group();
-        var a = space * 0.95;
-        var mesh = new THREE.Mesh(
-            new THREE.BoxGeometry( a, h, a),
-            new THREE.MeshStandardMaterial({ color: color, map: texture_rnd1 }) );
-        mesh.position.y = h / 2;
-        //mesh.rotation.y = Math.PI / 180 * 20 * -1;
-        var ground = new THREE.Mesh(
-            new THREE.BoxGeometry( space, 0.1, space),
-            new THREE.MeshStandardMaterial({ color: 0xffffff, map: texture_rnd1}) );
-        ground.position.y = 0.05 * -1;
-        box.add(mesh)  
-        box.add(ground);
-        return box;
-    };
-    var array_source_objects = [
-        mkBox(0x00ffff, 0.25), //new THREE.Object3D(),
-        mkBox(0xff0000, 10.00),
-        mkBox(0xffff00, 4.50),
-        mkBox(0x00ff00, 1.50)
-    ];
-
-    var array_oi = [
-        0,0,0,0,0,3,3,0,0,
-        0,0,0,0,3,2,3,0,0,
-        0,0,0,3,2,3,3,0,0,
-        0,0,3,2,2,2,3,0,0,
-        0,3,2,2,1,2,3,0,0,
-        3,2,3,2,2,2,2,3,0,
-        0,3,0,3,3,3,2,3,0,
-        0,0,0,0,0,0,3,3,0,
-        0,0,0,0,0,0,0,0,0
-    ]
-    //******** **********
-    // CREATE GRID
-    //******** **********
-    var grid = ObjectGridWrap.create({
-        space: space,
-        tw: tw,
-        th: th,
-        //aOpacity: 1.25,
-        dAdjust: 1.25,
-        effects: ['opacity', 'scale', 'rotationB'],
-        sourceObjects: array_source_objects,
-        objectIndices: array_oi
-    });
-    scene.add(grid);
+//******** **********
+// GRID OPTIONS
+//******** **********
+var tw = 9,
+th = 15,
+space = 3;
+// source objects
+var mkGround = function(){
+    var ground = new THREE.Mesh(
+        new THREE.BoxGeometry( space, 0.1, space),
+        new THREE.MeshStandardMaterial({ color: 0xffffff}) );
+    ground.position.y = 0.05 * -1;
+    return ground;
+};
+var mkBox = function(color, h){
+    var box = new THREE.Group();
+    var a = space * 0.5;
+    var mesh = new THREE.Mesh(
+        new THREE.BoxGeometry( a, h, a),
+        new THREE.MeshStandardMaterial({ color: color}) );
+    mesh.position.y = h / 2;
+    //mesh.rotation.y = Math.PI / 180 * 20 * -1;
+    var ground = mkGround();
+    box.add(mesh)  
+    box.add(ground);
+    return box;
+};
+var array_source_objects = [
+    mkGround(),
+    mkBox(0x00ffff, 0.50),
+    mkBox(0xff00ff, 1.00),
+    mkBox(0x0000ff, 1.50),
+    mkBox(0x00ff00, 2.00),
+    mkBox(0xffff00, 2.50),
+    mkBox(0xff8f00, 3.50),
+    mkBox(0xff0000, 4.00),
+    mkBox(0xffffff, 7.00)
+];
+var array_oi = [
+0,0,0,2,4,0,0,0,8,
+0,0,0,1,4,0,0,0,7,
+0,0,0,1,5,0,0,8,7,
+0,0,0,2,5,7,0,0,7,
+0,0,0,2,6,0,0,0,8,
+0,0,0,1,5,7,0,0,7,
+0,0,0,3,5,0,0,0,7,
+0,0,0,1,4,7,0,0,8,
+0,0,0,2,4,0,0,0,7,
+0,0,0,3,4,0,0,0,7,
+0,0,0,2,4,0,0,0,7,
+0,0,0,1,4,0,0,0,8,
+0,0,0,1,4,7,0,0,7,
+0,0,0,2,4,0,0,8,7,
+0,0,0,3,4,0,0,0,7
+]
+//******** **********
+// CREATE GRID
+//******** **********
+var grid = ObjectGridWrap.create({
+    space: space,
+    tw: tw,
+    th: th,
+    //aOpacity: 1.25,
+    dAdjust: 1.25,
+    effects: ['opacity'],
+    sourceObjects: array_source_objects,
+    objectIndices: array_oi
+});
+scene.add(grid);
+//******** **********
+// WERID WALK THREE
+//******** **********
+var ww3_1 = WeirdWalk.create();
+var s = 0.5;
+ww3_1.scale.set(s, s, s);
+ww3_1.position.set(-7, 2.7, -3);
+scene.add(ww3_1);
 
 
     // A SEQ FOR TEXT CUBE
@@ -147,8 +170,15 @@ VIDEO.init = function(sm, scene, camera){
         fps: 30,
         beforeObjects: function(seq){
 
-            ObjectGridWrap.setPos(grid, (1 - seq.per) * 2, Math.cos(Math.PI * seq.bias) * 0.25 );
-            ObjectGridWrap.update(grid);
+            //ObjectGridWrap.setPos(grid, (1 - seq.per) * 2, Math.cos(Math.PI * seq.bias) * 0.25 );
+            //ObjectGridWrap.update(grid);
+
+
+        ObjectGridWrap.setPos(grid, 0.10 - 0.10 * seq.bias, seq.per * 1 );
+        ObjectGridWrap.update(grid);
+
+        ww3_1.userData.legs.rotation.x = -Math.PI * 4 * seq.per;
+        ww3_1.userData.legs.rotation.z = Math.PI / 180 * 20;
 
             textCube.visible = false;
             camera.position.set(8, 1, 0);
