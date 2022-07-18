60361520d352059a2ed6eed7aae802068adbd7f5
dustinpfister <dustin.pfister@gmail.com>
2022-07-18 15:42:43 -0400

    added object-grid-wrap-r2 to js folder

--- videos/threejs-examples-object-grid-wrap-land/videos/video1.js
+++ videos/threejs-examples-object-grid-wrap-land/videos/video1.js
@@ -6,7 +6,9 @@ VIDEO.scripts = [
    '../../../js/canvas-text-cube.js',
    '../../../js/sequences-hooks-r1.js',
    '../../../js/datatex.js',
-   '../../../js/object-grid-wrap-r1.js'
+   '../../../js/object-grid-wrap-r2.js',
+   '../../../js/object-grid-wrap-r2-effects-opacity2.js',
+   '../js/object-grid-wrap-land.js'
 ];
 // init
 VIDEO.init = function(sm, scene, camera){
@@ -45,66 +47,100 @@ VIDEO.init = function(sm, scene, camera){
     dl.position.set(2, 1, 3);
     scene.add(dl);
 
-    //******** **********
-    // TEXTURES
-    //******** **********
-    var texture_rnd1 = datatex.seededRandom(40, 40, 1, 1, 1, [0, 255]);
 
-    //******** **********
-    // GRID OPTIONS
-    //******** **********
-    var tw = 9,
-    th = 9,
-    space = 1.5;
-    // source objects
-    var mkBox = function(color, h){
-        var box = new THREE.Group();
-        var a = space * 0.95;
-        var mesh = new THREE.Mesh(
-            new THREE.BoxGeometry( a, h, a),
-            new THREE.MeshStandardMaterial({ color: color, map: texture_rnd1 }) );
-        mesh.position.y = h / 2;
-        //mesh.rotation.y = Math.PI / 180 * 20 * -1;
-        var ground = new THREE.Mesh(
-            new THREE.BoxGeometry( space, 0.1, space),
-            new THREE.MeshStandardMaterial({ color: 0xffffff, map: texture_rnd1}) );
-        ground.position.y = 0.05 * -1;
-        box.add(mesh)  
-        box.add(ground);
-        return box;
-    };
-    var array_source_objects = [
-        mkBox(0x00ffff, 0.25), //new THREE.Object3D(),
-        mkBox(0xff0000, 10.00),
-        mkBox(0xffff00, 4.50),
-        mkBox(0x00ff00, 1.50)
-    ];
-
-    var array_oi = [
-        0,0,0,0,0,3,3,0,0,
-        0,0,0,0,3,2,3,0,0,
-        0,0,0,3,2,3,3,0,0,
-        0,0,3,2,2,2,3,0,0,
-        0,3,2,2,1,2,3,0,0,
-        3,2,3,2,2,2,2,3,0,
-        0,3,0,3,3,3,2,3,0,
-        0,0,0,0,0,0,3,3,0,
-        0,0,0,0,0,0,0,0,0
+//******** **********
+// GRID
+//******** **********
+var grid = ObjectGridWrapLand.create({
+    tw: 14,
+    th: 14,
+    crackSize: 0,
+    //effects:[],
+    altitude: [
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,1,1,1,0,0,0,1,1,1,1,0,
+        0,0,0,1,1,1,0,0,0,1,1,1,1,0,
+        0,0,0,1,1,1,0,0,0,1,1,1,1,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,1,1,1,1,0,0,0,0,0,0,0,
+        0,1,1,1,1,1,1,0,0,0,0,0,0,0,
+        0,1,2,2,2,1,1,0,0,0,0,0,0,0,
+        0,1,2,2,2,1,1,0,0,0,1,1,1,0,
+        0,1,2,2,2,1,1,0,0,0,1,1,1,0,
+        0,1,1,1,1,1,0,0,0,0,1,1,1,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    ],
+    objectIndices: [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 7, 4, 6, 0, 0, 0, 7, 4, 4, 6, 0,
+        0, 0, 0, 1, 0, 3, 0, 0, 0, 1, 0, 0, 3, 0,
+        0, 0, 0, 8, 2, 5, 0, 0, 0, 8, 2, 2, 5, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 7, 4, 4, 6, 0, 0, 0, 0, 0, 0, 0,
+        0, 7, 4, 9, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 7, 4, 6, 0, 3, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 1, 0, 3, 0, 3, 0, 0, 0, 7, 4, 6, 0,
+        0, 1, 8, 2, 5,11, 5, 0, 0, 0, 1, 0, 3, 0,
+        0, 8, 2, 2, 2, 5, 0, 0, 0, 0, 8, 2, 5, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]
-    //******** **********
-    // CREATE GRID
-    //******** **********
-    var grid = ObjectGridWrap.create({
-        space: space,
-        tw: tw,
-        th: th,
-        //aOpacity: 1.25,
-        dAdjust: 1.25,
-        effects: ['opacity', 'scale', 'rotationB'],
-        sourceObjects: array_source_objects,
-        objectIndices: array_oi
-    });
-    scene.add(grid);
+});
+
+grid.scale.set(1, 1, 1.25);
+scene.add(grid);
+//******** **********
+// ADDING CHILD MESH OBJECTS
+//******** **********
+var mkCone = function(height){
+    return new THREE.Mesh(
+        new THREE.ConeGeometry(0.5, height, 30, 30),
+        new THREE.MeshStandardMaterial({color: new THREE.Color('#00ff88')})
+    );
+};
+// can make another system that involves a grid if index values
+// but with child objects
+var mkMeshFunctions = [
+    null,
+    function(){
+        return mkCone(2)
+    },
+    function(){
+        return mkCone(3)
+    },
+    function(){
+        return mkCone(4)
+    }
+];
+// object index grid
+[
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,2,0,0,0,0,0,0,0,1,0,0,
+    0,0,1,0,0,0,3,0,0,1,2,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,1,0,1,1,0,0,
+    0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,1,0,1,2,1,0,0,
+    0,0,2,0,0,0,0,0,0,2,0,0,0,0,
+    0,0,0,1,0,0,0,2,1,0,1,1,0,0,
+    0,0,1,0,0,0,0,1,0,1,3,3,0,1,
+    0,1,0,1,0,1,2,0,1,2,1,1,2,0,
+    0,0,0,0,2,0,0,1,0,3,1,1,0,0,
+    0,1,0,1,0,1,0,0,0,1,2,3,1,1,
+    0,0,0,0,0,0,0,0,0,0,1,0,1,0
+].forEach(function(objIndex, i){
+    var mkMesh = mkMeshFunctions[objIndex];
+    if(mkMesh){
+        var mesh = mkMesh(),
+        x = i % grid.userData.tw,
+        y = Math.floor(i / grid.userData.tw)
+        ObjectGridWrapLand.addAt(grid, mesh, x, y);
+    }
+});
+    
 
 
     // A SEQ FOR TEXT CUBE
@@ -114,7 +150,7 @@ VIDEO.init = function(sm, scene, camera){
         beforeObjects: function(seq){
             var textCube = scene.userData.textCube;
             textCube.rotation.y = 0;
-            textCube.position.set(6, 0.8, 0);
+            textCube.position.set(6, 5, 0);
             textCube.visible = false;
             textCube.material.transparent = true;
             textCube.material.opacity = 0.0;
@@ -125,7 +161,7 @@ VIDEO.init = function(sm, scene, camera){
                 update: function(seq, partPer, partBias){
                     // text cube
                     textCube.visible = true;
-                    textCube.position.set(6, 0.8, 0);
+                    textCube.position.set(6, 5, 0);
                     textCube.material.opacity = 1.0;
                 }
             },
@@ -134,7 +170,7 @@ VIDEO.init = function(sm, scene, camera){
                 update: function(seq, partPer, partBias){
                     // move up text cube
                     textCube.visible = true;
-                    textCube.position.set(6, 0.8 + 1 * partPer, 0);
+                    textCube.position.set(6, 5 + 1 * partPer, 0);
                     textCube.rotation.y = Math.PI * 2 * partPer;
                     textCube.material.opacity = 1.0 - partPer;
                 }
@@ -147,11 +183,11 @@ VIDEO.init = function(sm, scene, camera){
         fps: 30,
         beforeObjects: function(seq){
 
-            ObjectGridWrap.setPos(grid, (1 - seq.per) * 2, Math.cos(Math.PI * seq.bias) * 0.25 );
+            ObjectGridWrap.setPos(grid, (1 - seq.per) * 2, 0 );
             ObjectGridWrap.update(grid);
 
             textCube.visible = false;
-            camera.position.set(8, 1, 0);
+            camera.position.set(8, 5, 0);
         },
         afterObjects: function(seq){
         },
@@ -164,15 +200,15 @@ VIDEO.init = function(sm, scene, camera){
                         seqHooks.setFrame(seq_textcube, seq.partFrame, seq.partFrameMax);
                     }
                     // camera
-                    camera.lookAt(0, 0, 0);
+                    camera.lookAt(0, 5, 0);
                 }
             },
             {
                 secs: 7,
                 update: function(seq, partPer, partBias){
                     // camera
-                    camera.position.set(8, 1 + 7 * partPer, 8 * partPer);
-                    camera.lookAt(0, 0, 0);
+                    camera.position.set(8, 5 + 3 * partPer, 8 * partPer);
+                    camera.lookAt(0, 5 - 5 * partPer, 0);
                 }
             },
             {
