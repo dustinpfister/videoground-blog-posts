60cde9276ccee39c6e01e2cce26e5d165f021146
dustinpfister <dustin.pfister@gmail.com>
2022-08-22 10:13:44 -0400

    new tiny-house-1 template file

---
+++ templates/template5-tiny-house-1/README.md
@@ -0,0 +1,3 @@
+# template 5 using the tiny house 1 dae file
+
+A template in which I am using my tiny-house-1 dae file as a subject to look at
---
+++ templates/template5-tiny-house-1/videos/video1.js
@@ -0,0 +1,161 @@
+// video1 for template5-tiny-house-1
+ 
+// scripts
+VIDEO.scripts = [
+   '../../../js/canvas/r0/canvas.js',
+   '../../../js/canvas-text-cube/r0/canvas-text-cube.js',
+   '../../../js/sequences-hooks/r1/sequences-hooks.js'
+];
+//******** **********
+// dae files
+//******** **********
+VIDEO.daePaths = [
+  '../../../dae/tiny-house-1/tiny-house-1-solid.dae'
+];
+// init
+VIDEO.init = function(sm, scene, camera){
+ 
+    //******** **********
+    // ADDING CHILD MESH OBJECTS FROM DAE FILE
+    //******** **********
+    var dscene = VIDEO.daeResults[0].scene;
+    var sourceObj = {};
+    var th1 = sourceObj.tree1 = dscene.getObjectByName('tiny-house-1');
+    th1.position.set(0, 0, 0);
+    scene.add(th1);
+
+    //******** **********
+    // LIGHT
+    //******** **********
+    var dl = new THREE.DirectionalLight(0xffffff, 1);
+    dl.position.set(2, 1, 3);
+    scene.add( new THREE.AmbientLight(0xffffff, 0.2))
+    scene.add(dl);
+ 
+    // BACKGROUND
+    scene.background = new THREE.Color('#2a2a2a');
+
+    // GRID
+    var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    grid.material.linewidth = 3;
+    scene.add( grid );
+ 
+    // TEXT CUBE
+    var textCube = scene.userData.textCube = CanvasTextCube.create({
+        width: 128,
+        height: 128,
+        lineWidth: 7,
+        lineColor: 'rgba(0,100,128,0.8)',
+        lineCount: 9,
+        lines: [
+            ['template2', 64, 17, 14, 'white'],
+            ['', 64, 32, 14, 'white'],
+            ['', 64, 47, 14, 'white'],
+            ['( r140 dd/mm/yyyy )', 64, 70, 12, 'gray'],
+            ['video1', 64, 100, 10, 'gray']
+        ]
+    });
+    scene.add(textCube);
+
+    // A SEQ FOR TEXT CUBE
+    var seq_textcube = seqHooks.create({
+        setPerValues: false,
+        fps: 30,
+        beforeObjects: function(seq){
+            var textCube = scene.userData.textCube;
+            textCube.rotation.y = 0;
+            textCube.position.set(10, 5.0, 0);
+            textCube.visible = false;
+            textCube.material.transparent = true;
+            textCube.material.opacity = 0.0;
+        },
+        objects: [
+            {
+                per: 0,
+                update: function(seq, partPer, partBias){
+                    // text cube
+                    textCube.visible = true;
+                    textCube.material.opacity = 1.0;
+                }
+            },
+            {
+                per: 0.75,
+                update: function(seq, partPer, partBias){
+                    // move up text cube
+                    textCube.visible = true;
+                    textCube.position.set(10 - 5 * partPer, 5.0 + 1 * partPer, 0);
+                    textCube.rotation.y = Math.PI * 2 * partPer;
+                    textCube.material.opacity = 1.0 - partPer;
+                }
+            }
+        ]
+    });
+
+    // A MAIN SEQ OBJECT
+    var seq = scene.userData.seq = seqHooks.create({
+        fps: 30,
+        beforeObjects: function(seq){
+            textCube.visible = false;
+            camera.position.set(12, 5, 0);
+        },
+        afterObjects: function(seq){
+        },
+        objects: [
+            {
+                secs: 3,
+                update: function(seq, partPer, partBias){
+                    // textcube
+                    if(seq.partFrame < seq.partFrameMax){
+                        seqHooks.setFrame(seq_textcube, seq.partFrame, seq.partFrameMax);
+                    }
+                    // camera
+                    camera.lookAt(0, 5, 0);
+                }
+            },
+            {
+                secs: 7,
+                update: function(seq, partPer, partBias){
+                    // camera
+                    camera.position.set(12 + 3 * partPer, 5, 0);
+                    camera.lookAt(0, 5 - 1 * partPer, 0);
+                }
+            },
+            {
+                secs: 7,
+                update: function(seq, partPer, partBias){
+                    // camera
+                    camera.position.set(15, 5, 0);
+                    camera.lookAt(0, 4, 0);
+                }
+            },
+            {
+                secs: 7,
+                update: function(seq, partPer, partBias){
+                    // camera
+                    camera.position.set(15, 5 + 10 * partPer, 15 * partPer);
+                    camera.lookAt(0, 4, 0);
+                }
+            },
+            {
+                secs: 7,
+                update: function(seq, partPer, partBias){
+                    // camera
+                    camera.position.set(15, 15, 15);
+                    camera.lookAt(0, 4, 0);
+                }
+            }
+        ]
+    });
+
+    console.log('frameMax for main seq: ' + seq.frameMax);
+    sm.frameMax = seq.frameMax;
+
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    var seq = scene.userData.seq;
+    seqHooks.setFrame(seq, sm.frame, sm.frameMax);
+
+};
+
