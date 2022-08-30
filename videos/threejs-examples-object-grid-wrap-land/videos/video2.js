6ce0c62007238712e92251fed2e4072c9a74e683
dustinpfister <dustin.pfister@gmail.com>
2022-08-30 17:53:49 -0400

    looks like I have my child objects working in the video

--- videos/threejs-examples-object-grid-wrap-land/videos/video2.js
+++ videos/threejs-examples-object-grid-wrap-land/videos/video2.js
@@ -117,6 +117,85 @@ var gridOpt = {
     scene.add(grid);
 
 
+    //******** **********
+    // ADDING CHILD MESH OBJECTS
+    //******** **********
+    // get a random radian
+    var getRandomRadian = function(){
+        return Math.PI * 2 * THREE.MathUtils.seededRandom();
+    };
+    var setRandomScale = function(mesh, sMin, sMax){
+        var scale = sMin + ( sMax - sMin ) * THREE.MathUtils.seededRandom();
+        mesh.scale.set(scale, scale, scale);
+    };
+    // can make another system that involves a grid of index values
+    // but with child objects
+    var mkMeshFunctions = [
+        null,
+        function(){
+            var mesh = sObj.tree_1.clone();
+            mesh.material = sObj.tree_1.material.clone();
+            setRandomScale(mesh, 0.25, 0.75);
+            return mesh;
+        },
+        function(){
+            var mesh = sObj.tree_2.clone();
+            mesh.material = sObj.tree_2.material.clone();
+            setRandomScale(mesh, 0.25, 0.75);
+            mesh.rotation.y = getRandomRadian()
+            return mesh;
+        },
+        function(){
+            var mesh = sObj.tree_3.clone();
+            mesh.material = sObj.tree_3.material.clone();
+            setRandomScale(mesh, 0.35, 0.50);
+            mesh.rotation.y = getRandomRadian()
+            return mesh;
+        }
+    ];
+    // object index grid
+    [
+        2,2,0,0,0,0,0,0,0,0,2,2,1,2,
+        2,1,2,2,0,0,0,0,0,0,0,1,2,0,
+        0,2,1,2,2,0,3,0,0,1,2,2,1,2,
+        0,0,2,2,2,2,0,0,0,0,3,2,2,2,
+        0,0,0,2,2,0,0,0,1,0,1,1,2,2,
+        0,0,0,1,2,2,2,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,1,0,1,2,1,0,0,
+        0,0,2,0,0,0,0,0,0,2,0,0,0,0,
+        0,0,0,1,0,0,0,2,1,0,1,1,0,0,
+        0,0,1,0,0,0,0,1,0,1,3,3,0,1,
+        0,1,0,1,0,1,2,0,1,2,1,1,2,0,
+        0,0,0,0,2,0,0,1,0,3,1,1,2,2,
+        2,1,0,1,0,1,0,0,2,1,2,3,1,1,
+        2,2,0,0,0,0,0,0,0,2,1,2,1,2
+    ].forEach(function(objIndex, i){
+        var mkMesh = mkMeshFunctions[objIndex];
+        if(mkMesh){
+            var mesh = mkMesh(),
+            x = i % grid.userData.tw,
+            y = Math.floor(i / grid.userData.tw);
+            // add at method
+            ObjectGridWrapLand.addAt(grid, mesh, x, y, function(tile, mesh, tileUD){
+                // default to just the alt value of the tile
+                mesh.position.y = tileUD.alt;
+                // if tile is a cube
+                if(tileUD.isCube){
+                    mesh.position.y = tileUD.alt + 0.25;
+                }
+                // if tile is a cube
+                if(tileUD.isSlope){
+                    mesh.position.y = tileUD.alt - 0.25;
+                }
+                // if tile is a cube
+                if(tileUD.isCorner){
+                    mesh.position.y = tileUD.alt - 0.25;
+                }
+            });
+        }
+    });
+
+
     // A SEQ FOR TEXT CUBE
     var seq_textcube = seqHooks.create({
         setPerValues: false,
