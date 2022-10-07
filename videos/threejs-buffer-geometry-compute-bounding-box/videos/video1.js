daca61f63d5cf9ab33cba02bd398264ef80d6a72
dustinpfister <dustin.pfister@gmail.com>
2022-10-07 11:49:58 -0400

    basic idea working

--- videos/threejs-buffer-geometry-compute-bounding-box/videos/video1.js
+++ videos/threejs-buffer-geometry-compute-bounding-box/videos/video1.js
@@ -8,6 +8,66 @@ VIDEO.scripts = [
 ];
 // init
 VIDEO.init = function(sm, scene, camera){
+//-------- ----------
+// HELPERS
+//-------- ----------
+const getMeshGroundPosition = (mesh, x, z) => {
+    const geo = mesh.geometry;
+    // COMPUTE THE BOUNDING BOX AND GET bb REF TO IT
+    geo.computeBoundingBox();
+    const bb = geo.boundingBox;
+    // GET SIZE, and return new Vector3
+    const v_size = new THREE.Vector3();
+    bb.getSize(v_size);
+    return new THREE.Vector3(x, v_size.y / 2, z);
+};
+// Make Mesh
+
+const makeMesh = (w, h, d, x, z, sh, p1, p2, m) => {
+    const mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(w, h, d), m);
+    mesh.userData.v_start = new THREE.Vector3(x, sh, z);
+    mesh.userData.p1 = p1;
+    mesh.userData.p2 = p2;
+    return mesh
+};
+// set mesh animation state for the given alpha
+const setMesh = (mesh, alpha) => {
+    let mud = mesh.userData;
+    let b = getAlpha(alpha, 1, mud.p1, mud.p2);
+    let v_start = mud.v_start;
+    let v_ground = getMeshGroundPosition(mesh, v_start.x, v_start.z);
+    mesh.position.copy(v_start).lerp(v_ground, b);
+};
+// get alpha helper
+const getAlpha = (n, d, p1, p2) => {
+    let a = n / d;
+    let b = 0;
+    if(a < p1){ b = a * (1 / p1);}
+    if(a >= p1 && a < p2){ b = 1;}
+    if(a >= p2){
+        b = (1 - a) / (1 - p2);
+    }
+    return b;
+};
+//-------- ----------
+// GROUP, MESH, MATERIAL
+//-------- ----------
+const material = new THREE.MeshNormalMaterial();
+let group = new THREE.Group();
+[
+    [1, 1, 1, 0.5, 0.5, 12, 0.65, 0.8, material],
+    [1, 3, 1, -4.5, -4.5, 10, 0.75, 0.9, material],
+    [1, 3.25, 3, -4.5, 0.5, 8, 0.25, 0.5, material],
+    [3, 1, 2, 3, 3, 8, 0.90, 0.95, material],
+    [2, 2, 2, 3, -2, 8, 0.15, 0.4, material]
+].forEach((argu) => {
+    let mesh = makeMesh.apply(null, argu);
+    group.add(mesh);
+});
+scene.add(group);
+
+
     //-------- ----------
     // BACKGROUND
     //-------- ----------
@@ -73,11 +133,17 @@ VIDEO.init = function(sm, scene, camera){
     //-------- ----------
     // A MAIN SEQ OBJECT
     //-------- ----------
+const update = function(a){
+    group.children.forEach((mesh)=>{
+        setMesh(mesh, a);
+    });
+};
     var seq = scene.userData.seq = seqHooks.create({
         fps: 30,
         beforeObjects: function(seq){
             textCube.visible = false;
             camera.position.set(8, 1, 0);
+update(seq.per * 2 % 1);
         },
         afterObjects: function(seq){
         },
