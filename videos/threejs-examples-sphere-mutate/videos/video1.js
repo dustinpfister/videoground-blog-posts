3adf867847b3cbf1c3d0dce73c2801f2219a60b6
dustinpfister <dustin.pfister@gmail.com>
2022-10-11 10:56:47 -0400

    video1 for the sphere mutate threejs examples post looks okay

--- videos/threejs-examples-sphere-mutate/videos/video1.js
+++ videos/threejs-examples-sphere-mutate/videos/video1.js
@@ -9,6 +9,87 @@ VIDEO.scripts = [
  
 // init
 VIDEO.init = function(sm, scene, camera){
+    // ---------- ----------
+    // LIGHT
+    // ---------- ----------
+    const dl = new THREE.DirectionalLight(0xffffff, 1);
+    dl.position.set(-3, 1, 2);
+    scene.add(dl);
+    const al = new THREE.AmbientLight(0xffffff, 0.1);
+    scene.add(al);
+    // ---------- ----------
+    // HELPERS
+    // ---------- ----------
+    // create new vectors
+    const createNewVectors = (mesh, mag) => {
+        mag = mag === undefined ? 0.25 : mag
+        const pos = mesh.userData.pos_base;
+        const len = pos.count, vstart=[], vend= [];
+        let i = 0;
+        while(i < len){
+            const v = new THREE.Vector3(pos.getX(i), pos.getY(i), pos.getZ(i));
+            const ul = v.length();
+            vstart.push(v);
+            vend.push(v.clone().normalize().multiplyScalar(ul - mag + mag * 2 * Math.random()));
+            i += 1;
+        }
+        mesh.userData.vstart = vstart;
+        mesh.userData.vend = vend;
+    };
+    // create the mesh object
+    const createMesh = () => {
+        const mesh = new THREE.Mesh(
+            new THREE.SphereGeometry(3.25, 40, 40, 0, Math.PI * 1.6), 
+            new THREE.MeshPhongMaterial({
+                color: 'white',
+                map: texture,
+                side: THREE.DoubleSide
+            }));
+        mesh.userData.pos_base = mesh.geometry.getAttribute('position').clone();
+        createNewVectors(mesh);
+        return mesh;
+    };
+    // update the mesh object
+    const updateMeshGeo = (mesh, alpha) => {
+        const geo = mesh.geometry;
+        const pos = geo.getAttribute('position');
+        const len = pos.count;
+        const mud = mesh.userData;
+        let i = 0;
+        while(i < len){
+            const v = mud.vstart[i].clone().lerp( mud.vend[i], alpha );
+            pos.array[i * 3] = v.x;
+            pos.array[i * 3 + 1] = v.y;
+            pos.array[i * 3 + 2] = v.z;
+            i += 1;
+        }
+        pos.needsUpdate = true;
+    };
+    //-------- ----------
+    // TEXTURE
+    //-------- ----------
+    // USING THREE DATA TEXTURE To CREATE A RAW DATA TEXTURE
+    const width = 32, height = 32;
+    const size = width * height;
+    const data = new Uint8Array( 4 * size );
+    for ( let i = 0; i < size; i ++ ) {
+        const stride = i * 4;
+        const a1 = i / size;
+        const a2 = i % width / width;
+        const v = 30 + 70 * Math.random();
+        data[ stride ] = v + 150 * a1;           // red
+        data[ stride + 1 ] = v + 150 - 150 * a1; // green
+        data[ stride + 2 ] = v + 50 * a2;        // blue
+        data[ stride + 3 ] = 255;                // alpha
+    }
+    const texture = new THREE.DataTexture( data, width, height );
+    texture.needsUpdate = true;
+    // ---------- ----------
+    // GEOMETRY, MESH
+    // ---------- ----------
+    const mesh = createMesh();
+    scene.add(mesh);
+    camera.lookAt(mesh.position);
     //-------- ----------
     // BACKGROUND
     //-------- ----------
@@ -74,9 +155,24 @@ VIDEO.init = function(sm, scene, camera){
     //-------- ----------
     // A MAIN SEQ OBJECT
     //-------- ----------
+    const MAGS = [
+        [0.75, 1.5],
+        [0.25, 0.35],
+        [1.25, 2.25]
+    ];
     const seq = scene.userData.seq = seqHooks.create({
         fps: 30,
         beforeObjects: function(seq){
+            let alpha = seq.per;
+            let bias = seq.getSinBias(6, false);
+            if(bias <= 0.05){
+                const mi = Math.floor(  MAGS.length * seq.per);
+                const magMin = MAGS[mi][0];
+                const magMax = MAGS[mi][1];
+                createNewVectors(mesh, magMin + (magMax - magMin) * Math.random())
+            }
+            updateMeshGeo(mesh, bias);
+            mesh.rotation.y = Math.PI * 4 * alpha;
             textCube.visible = false;
             camera.position.set(8, 1, 0);
         },
@@ -106,7 +202,7 @@ VIDEO.init = function(sm, scene, camera){
                 secs: 20,
                 update: function(seq, partPer, partBias){
                     // camera
-                    camera.position.set(8, 8, 8);
+                    camera.position.set(8 - 16 * partPer, 8, 8);
                     camera.lookAt(0, 0, 0);
                 }
             }
