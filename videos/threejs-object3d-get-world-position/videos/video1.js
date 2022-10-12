919c41a95ece7820d0e4ef6e793a96ef4da4f8b2
dustinpfister <dustin.pfister@gmail.com>
2022-10-12 12:56:24 -0400

    video1 code looks good

--- videos/threejs-object3d-get-world-position/videos/video1.js
+++ videos/threejs-object3d-get-world-position/videos/video1.js
@@ -1,186 +1,183 @@
 // video1 for threejs-object3d-get-world-position
-
+ 
 // scripts
 VIDEO.scripts = [
-   '../../../js/canvas.js',
-   '../../../js/canvas-text-cube.js',
-   '../../../js/sequences.js'
+   '../../../js/sequences-hooks/r1/sequences-hooks.js',
+   '../../../js/canvas/r0/canvas.js',
+   '../../../js/canvas-text-cube/r0/canvas-text-cube.js'
 ];
-
-var createGroup = function (color) {
+// init
+VIDEO.init = function(sm, scene, camera){
+//-------- ----------
+// HELPERS
+//-------- ----------
+const createGroup = function (color, x) {
     color = color || new THREE.Color(1, 1, 1);
-    // creating a group
-    var group = new THREE.Group();
-    // creating and adding a pointer mesh to the group
-    var geo = new THREE.CylinderGeometry(0, 0.5, 1, 12);
+    const group = new THREE.Group();
+    const geo = new THREE.CylinderGeometry(0, 0.75, 2.25, 12);
     geo.rotateX(Math.PI * 0.5);
-    var pointer = group.userData.pointer = new THREE.Mesh(
+    const pointer = group.userData.pointer = new THREE.Mesh(
             geo,
             new THREE.MeshNormalMaterial());
     pointer.position.set(0, 0, 0);
-    pointer.rotation.y = 1.57; // BY DEFAULT THE POINTER IS NOT POINTING AT THE CUBE
+    pointer.rotation.y = 1.57;
     group.add(pointer);
-    // creating and adding a cube
-    var cube = group.userData.cube = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
+    const cube = group.userData.cube = new THREE.Mesh(
+            new THREE.BoxGeometry(0.75, 0.75, 0.75),
             new THREE.MeshBasicMaterial({ color: color, transparent: true, opacity: 0.5 }));
     cube.position.set(0, 0, 1);
     group.add(cube);
-    // box helper for the group
-    group.add(new THREE.BoxHelper(group));
+    group.position.set(x, 0, 0);
     return group;
 };
- 
-// init method for the video
-VIDEO.init = function(sm, scene, camera){
- 
-    // BACKGROUND
-    scene.background = new THREE.Color('#2a2a2a');
-    scene.add( new THREE.GridHelper(10, 10, '#ffffff', '#00afaf') );
- 
-    // LIGHT
-    var dl = new THREE.DirectionalLight(0xffffff, 1);
-    dl.position.set(1, 1, 1);
-    scene.add(dl);
+// update
+const updateGroup = (group, alpha) => {
+    const e = new THREE.Euler();
+    e.y = Math.PI * 2 * alpha;
+    group.userData.cube.position.copy( new THREE.Vector3(1,0,0) ).applyEuler(e).normalize().multiplyScalar(1.5);
+};
 
+//-------- ----------
+// SCENE CHILD OBJECTS
+//-------- ----------
+// just set up group1 and group2
+const group1 = createGroup(0xff0000, -2.75);
+scene.add(group1);
+const group2 = createGroup(0x00ff00, 2.75);
+scene.add(group2);
+
+const helper1 = new THREE.BoxHelper(group1);
+helper1.material.linewidth = 3;
+scene.add(helper1);
+const helper2 = new THREE.BoxHelper(group2);
+helper2.material.linewidth = 3;
+scene.add(helper2);
+    //-------- ----------
+    // BACKGROUND
+    //-------- ----------
+    scene.background = new THREE.Color('#2a2a2a');
+    //-------- ----------
+    // GRID
+    //-------- ----------
+    const grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    grid.material.linewidth = 3;
+    scene.add( grid );
+    //-------- ----------
     // TEXT CUBE
-    var textCube = scene.userData.textCube = CanvasTextCube.create({
+    //-------- ----------
+    const textCube = scene.userData.textCube = CanvasTextCube.create({
         width: 128,
         height: 128,
         lineWidth: 7,
         lineColor: 'rgba(0,100,128,0.8)',
         lineCount: 9,
         lines: [
-            ['Get World Position', 64, 20, 14, 'white'],
-            ['in Three.js.', 64, 45, 14, 'white'],
-            ['( r135 04/24/2022 )', 64, 70, 12, 'gray'],
+            ['Object3d Get World', 64, 17, 14, 'white'],
+            ['Position Method in', 64, 32, 14, 'white'],
+            ['threejs', 64, 47, 14, 'white'],
+            ['( r140 10/12/2022 )', 64, 70, 12, 'gray'],
             ['video1', 64, 100, 10, 'gray']
         ]
     });
     scene.add(textCube);
- 
-
-    var group = scene.userData.group = createGroup(0xff0000); // group 1
-    scene.add(group);
-    var group2 = scene.userData.group2 = createGroup(0x00ff00); // group2
-    scene.add(group2);
-
-
-    // freeCube at 0,0,11
-    var freeCube = scene.userData.freeCube = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshNormalMaterial());
-    freeCube.position.set(0, 0, 1);
-    scene.add(freeCube);
- 
-
-    // SET UP SEQ OBJECT
-    sm.seq = Sequences.create({
-        sm: sm,
-        part : [
+    // A SEQ FOR TEXT CUBE
+    const seq_textcube = seqHooks.create({
+        setPerValues: false,
+        fps: 30,
+        beforeObjects: function(seq){
+            const textCube = scene.userData.textCube;
+            textCube.rotation.y = 0;
+            textCube.position.set(6, 0.8, 0);
+            textCube.visible = false;
+            textCube.material.transparent = true;
+            textCube.material.opacity = 0.0;
+        },
+        objects: [
             {
                 per: 0,
-                init: function(sm){},
-                update: function(sm, scene, camera, partPer, partBias){
+                update: function(seq, partPer, partBias){
                     // text cube
                     textCube.visible = true;
-                    textCube.position.set(0, 0.8, 6);
-                    // camera
-                    camera.position.set(0, 1, 8);
-                    camera.lookAt(0, 0, 0);
+                    textCube.position.set(6, 0.8, 0);
+                    textCube.material.opacity = 1.0;
                 }
             },
             {
-                per: 0.10,
-                init: function(sm){},
-                update: function(sm, scene, camera, partPer, partBias){
+                per: 0.75,
+                update: function(seq, partPer, partBias){
                     // move up text cube
                     textCube.visible = true;
-                    textCube.position.set(0, 0.8 + 2 * partPer, 6);
+                    textCube.position.set(6, 0.8 + 1 * partPer, 0);
                     textCube.rotation.y = Math.PI * 2 * partPer;
-                    // camera
-                    camera.position.set(0, 1, 8);
-                    camera.lookAt(0, 0, 0);
-                }
-            },
-            {
-                per: 0.20,
-                init: function(sm){},
-                update: function(sm, scene, camera, partPer, partBias){
-                    // camera
-                    camera.position.set(0, 1 + 4 * partPer, 8);
-                    camera.lookAt(0, 0, 0);
-
-                    group.position.z = 2 * partPer;
-                    group2.position.z = -2 * partPer;
-
-                }
-            },
-            {
-                per: 0.40,
-                init: function(sm){},
-                update: function(sm, scene, camera, partPer, partBias){
-                    // camera
-                    camera.position.set(0, 5, 8);
-                    camera.lookAt(0, 0, 0);
-
-                    group.position.z = 2;
-                    group2.position.z = -2;
-
-                    group.rotation.set(0.0, Math.PI / 2 * partPer, 0.0);
-                    group2.rotation.set(0.0, Math.PI / 2 * partPer * -1, 0.0);
-
-                }
-            },
-            {
-                per: 0.60,
-                init: function(sm){},
-                update: function(sm, scene, camera, partPer, partBias){
-                    // camera
-                    camera.position.set(0, 5, 8);
-                    camera.lookAt(0, 0, 0);
-
-                    group.position.z = 2;
-                    group2.position.z = -2;
-
-                    group.position.x = -2 + 4 * partPer;
-                    group2.position.x = 2 - 4 * partPer;
-
-                    group.rotation.set(0.0, Math.PI / 2, 0.0);
-                    group2.rotation.set(0.0, Math.PI / 2 * -1, 0.0);
-
-                    
-
+                    textCube.material.opacity = 1.0 - partPer;
                 }
             }
         ]
     });
-};
-
-// update method for the video
-VIDEO.update = function(sm, scene, camera, per, bias){
-    var textCube = scene.userData.textCube;
-    textCube.rotation.y = 0;
-    textCube.position.set(0, 0, 6);
-    textCube.visible = false;
-
-    var group = scene.userData.group,
-    group2 = scene.userData.group2;
-
-    group.position.set(-2.0, 0, 0.0);
-    group.rotation.set(0.0, 0, 0.0);
-    group2.position.set(2.0, 0, 0.0);
-    group2.rotation.set(0.0, 0, 0.0);
-
-    // just look at the ube of the group
-    group.userData.pointer.lookAt(group.userData.cube.position);
- 
-    // use the getWorldPosition off of the cube to get the location in world space
-    var v = new THREE.Vector3(0, 0, 0);
+    //-------- ----------
+    // A MAIN SEQ OBJECT
+    //-------- ----------
+const update = function(a){
+    // animate groups the same way
+    updateGroup(group1, a);
+    updateGroup(group2, a);
+    helper1.update();
+    helper2.update();
+    // with group1 I am just passing lookAt the LOCAL position of the cube
+    group1.userData.pointer.lookAt(group1.userData.cube.position);
+    // with group to I am USING GETWORLDPOSITION to get a vector to pass to lookAt
+    const v = new THREE.Vector3(0, 0, 0);
     group2.userData.cube.getWorldPosition(v);
     group2.userData.pointer.lookAt(v);
-
-    // sequences
-    Sequences.update(sm.seq, sm);
 };
+    const seq = scene.userData.seq = seqHooks.create({
+        fps: 30,
+        beforeObjects: function(seq){
 
+update(seq.getSinBias(4, false));
+
+            textCube.visible = false;
+            camera.position.set(8, 1, 0);
+        },
+        afterObjects: function(seq){
+        },
+        objects: [
+            {
+                secs: 3,
+                update: function(seq, partPer, partBias){
+                    // textcube
+                    if(seq.partFrame < seq.partFrameMax){
+                        seqHooks.setFrame(seq_textcube, seq.partFrame, seq.partFrameMax);
+                    }
+                    // camera
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            {
+                secs: 7,
+                update: function(seq, partPer, partBias){
+                    // camera
+                    camera.position.set(8 - 2 * partPer, 1 + 6 * partPer, 6 * partPer);
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            {
+                secs: 20,
+                update: function(seq, partPer, partBias){
+                    // camera
+                    camera.position.set(6 + 3 * partPer, 6, 6 - 12 * seq.getSinBias(1));
+                    camera.lookAt(0, 0, 0);
+                }
+            }
+        ]
+    });
+    console.log('frameMax for main seq: ' + seq.frameMax);
+    sm.frameMax = seq.frameMax;
+};
+ 
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    const seq = scene.userData.seq;
+    seqHooks.setFrame(seq, sm.frame, sm.frameMax);
+};
+ 
