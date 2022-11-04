3d9e69e07c206c4b6cce47d2cdae155fa2ff674f
dustinpfister <dustin.pfister@gmail.com>
2022-11-04 13:30:01 -0400

    have a cube in the scene

--- videos/threejs-examples-uvmap-cube-canvas-update/videos/video1.js
+++ videos/threejs-examples-uvmap-cube-canvas-update/videos/video1.js
@@ -1,13 +1,72 @@
-// video1 for template2-sequence-hooks using r0 of canvas.js
- 
-// scripts
+// video1 for threejs-examples-uvmap-cube-canvas-update
 VIDEO.scripts = [
-   '../../../js/sequences-hooks/r1/sequences-hooks.js',
-   '../../../js/canvas/r0/canvas.js',
-   '../../../js/canvas-text-cube/r0/canvas-text-cube.js'
+   '../../../js/sequences-hooks/r2/sequences-hooks.js',
+   '../../../js/canvas/r1/canvas.js',
+   '../../../js/canvas-text-cube/r1/canvas-text-cube.js',
+   '../../../js/canvas-uvmap-cube/r0/uvmap-cube.js'
 ];
 // init
 VIDEO.init = function(sm, scene, camera){
+    //-------- ----------
+    // SOURCE CANVAS
+    //-------- ----------
+    const canOpt1 = {
+            draw: 'rnd', 
+            update_mode: 'canvas', 
+            palette: ['white', 'red'], 
+            size:128, 
+            state:{ gSize: 4 }
+    };
+    const canObj1 = canvasMod.create( canOpt1 );
+    //-------- ----------
+    // CREATE MESH
+    //-------- ----------
+    const mesh = uvMapCube.create({
+        pxa: 1.5,
+        images: [ 
+            canObj1.canvas
+        ]
+    });
+    scene.add(mesh);
+    uvMapCube.drawFace(mesh, 'front', {i:0, sx: 0, sy: 0, sw: 128, sh: 128});
+    //-------- ----------
+    // HELPERS
+    //-------- ----------
+/*
+    // just a short hand for THREE.QuadraticBezierCurve3
+    const QBC3 = function(x1, y1, z1, x2, y2, z2, x3, y3, z3){
+        let vs = x1;
+        let ve = y1;
+        let vc = z1;
+        if(arguments.length === 9){
+            vs = new THREE.Vector3(x1, y1, z1);
+            ve = new THREE.Vector3(x2, y2, z2);
+            vc = new THREE.Vector3(x3, y3, z3);
+        }
+        return new THREE.QuadraticBezierCurve3( vs, vc, ve );
+    };
+    // QBDelta helper using QBC3
+    // this works by giving deltas from the point that is half way between
+    // the two start and end points rather than a direct control point for x3, y3, and x3
+    const QBDelta = function(x1, y1, z1, x2, y2, z2, x3, y3, z3) {
+        const vs = new THREE.Vector3(x1, y1, z1);
+        const ve = new THREE.Vector3(x2, y2, z2);
+        // deltas
+        const vDelta = new THREE.Vector3(x3, y3, z3);
+        const vc = vs.clone().lerp(ve, 0.5).add(vDelta);
+        const curve = QBC3(vs, ve, vc);
+        return curve;
+    };
+    // QBV3Array
+    const QBV3Array = function(data) {
+        const v3Array = [];
+        data.forEach( ( a ) => {
+            const curve = QBDelta.apply(null, a.slice(0, a.length - 1))
+            v3Array.push( curve.getPoints( a[9]) );
+        })
+        return v3Array.flat();
+    };
+*/
     //-------- ----------
     // BACKGROUND
     //-------- ----------
@@ -19,19 +78,33 @@ VIDEO.init = function(sm, scene, camera){
     grid.material.linewidth = 3;
     scene.add( grid );
     //-------- ----------
+    // PATHS
+    //-------- ----------
+    /*
+    const v3Array_campos = QBV3Array([
+        [8,8,8, 7,-2,-7,    0,0,0,      20]
+    ]);
+    // PATH DEBUG POINTS
+    const points_debug = new THREE.Points(
+        new THREE.BufferGeometry().setFromPoints(v3Array_campos),
+        new THREE.PointsMaterial({size: 0.25, color: new THREE.Color(0,1,0)})
+    );
+    scene.add(points_debug);
+    */
+    //-------- ----------
     // TEXT CUBE
     //-------- ----------
     const textCube = scene.userData.textCube = CanvasTextCube.create({
-        width: 128,
-        height: 128,
+        bg: '#000000',
+        size: 128,
         lineWidth: 7,
-        lineColor: 'rgba(0,100,128,0.8)',
+        lineColor: 'rgba(0,100,0,0.8)',
         lineCount: 9,
         lines: [
-            ['template2', 64, 17, 14, 'white'],
-            ['', 64, 32, 14, 'white'],
-            ['', 64, 47, 14, 'white'],
-            ['( r140 dd/mm/yyyy )', 64, 70, 12, 'gray'],
+            ['UV map cube', 64, 17, 14, 'white'],
+            ['Threejs module', 64, 32, 14, 'white'],
+            ['Example', 64, 47, 14, 'white'],
+            ['( r140 11/04/2022 )', 64, 70, 12, 'gray'],
             ['video1', 64, 100, 10, 'gray']
         ]
     });
@@ -54,7 +127,6 @@ VIDEO.init = function(sm, scene, camera){
                 update: function(seq, partPer, partBias){
                     // text cube
                     textCube.visible = true;
-                    textCube.position.set(6, 0.8, 0);
                     textCube.material.opacity = 1.0;
                 }
             },
@@ -73,48 +145,81 @@ VIDEO.init = function(sm, scene, camera){
     //-------- ----------
     // A MAIN SEQ OBJECT
     //-------- ----------
-    const seq = scene.userData.seq = seqHooks.create({
+    // start options for main seq object
+    const opt_seq = {
         fps: 30,
         beforeObjects: function(seq){
             textCube.visible = false;
             camera.position.set(8, 1, 0);
+            camera.zoom = 1;
         },
         afterObjects: function(seq){
+            camera.updateProjectionMatrix();
         },
-        objects: [
-            {
-                secs: 3,
-                update: function(seq, partPer, partBias){
-                    // textcube
-                    if(seq.partFrame < seq.partFrameMax){
-                        seqHooks.setFrame(seq_textcube, seq.partFrame, seq.partFrameMax);
-                    }
-                    // camera
-                    camera.lookAt(0, 0, 0);
-                }
-            },
-            {
-                secs: 7,
-                update: function(seq, partPer, partBias){
-                    // camera
-                    camera.position.set(8, 1 + 7 * partPer, 8 * partPer);
-                    camera.lookAt(0, 0, 0);
-                }
-            },
-            {
-                secs: 20,
-                update: function(seq, partPer, partBias){
-                    // camera
-                    camera.position.set(8, 8, 8);
-                    camera.lookAt(0, 0, 0);
-                }
+        objects: []
+    };
+    // SEQ 0 - ...
+    opt_seq.objects[0] = {
+        secs: 3,
+        update: function(seq, partPer, partBias){
+            // textcube
+            if(seq.partFrame < seq.partFrameMax){
+               seqHooks.setFrame(seq_textcube, seq.partFrame, seq.partFrameMax);
             }
-        ]
-    });
+            // camera
+            //camera.position.set(5, 18, 14);
+            camera.lookAt(0, 0, 0);
+        }
+     };
+    // SEQ 1 - ...
+    opt_seq.objects[1] = {
+        secs: 2,
+        update: function(seq, partPer, partBias){
+            // camera
+            const v1 = new THREE.Vector3(8, 1, 0);
+            const v2 = new THREE.Vector3(2, 2, 2);
+            camera.position.copy( v1.lerp(v2, partPer) );
+            camera.lookAt(0, 0, 0);
+        }
+    };
+    // SEQ 2 - ...
+    opt_seq.objects[2] = {
+        secs: 25,
+        update: function(seq, partPer, partBias){
+            // camera
+            camera.position.set( 2, 2, 2 );
+            camera.lookAt(0, 0, 0);
+        }
+    };
+/*
+    // SEQ 2 - ...
+    opt_seq.objects[2] = {
+        secs: 5,
+        v3Paths: [
+            { key: 'campos', array: v3Array_campos, lerp: true }
+        ],
+        update: function(seq, partPer, partBias){
+            // camera
+            seq.copyPos('campos', camera);
+            camera.lookAt(0, 0, 0);
+        }
+    };
+    // SEQ 2 - ...
+    opt_seq.objects[3] = {
+        secs: 20,
+        update: function(seq, partPer, partBias){
+            // camera
+            camera.position.set(8 - 16 * partBias, 8, 8);
+            camera.lookAt(0, 0, 0);
+            const b2 = seq.getSinBias(1, true);
+            camera.zoom = 1 + 7 * b2;
+        }
+    };
+*/
+    const seq = scene.userData.seq = seqHooks.create(opt_seq);
     console.log('frameMax for main seq: ' + seq.frameMax);
     sm.frameMax = seq.frameMax;
 };
- 
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
     const seq = scene.userData.seq;
