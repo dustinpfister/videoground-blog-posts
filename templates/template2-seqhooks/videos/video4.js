d1783d43b17c64b94fd5a58a025e251d4beca861
dustinpfister <dustin.pfister@gmail.com>
2022-11-30 13:24:28 -0500

    now have a video4 for template2

--- templates/template2-seqhooks/videos/video4.js
+++ templates/template2-seqhooks/videos/video4.js
@@ -1,50 +1,13 @@
-// video3 for template2-sequence-hooks using r1 of canvas.js
-// and r2 of sequences-hooks as well as some helper functions
-// This is based off of the video3 that I made for the beta world counterpart
-// scripts
+// video4 for template2-sequence-hooks using r1 of canvas.js
+// and r2 of sequences-hooks. Also now using r0 of the curves.js module
 VIDEO.scripts = [
    '../../../js/sequences-hooks/r2/sequences-hooks.js',
    '../../../js/canvas/r1/canvas.js',
-   '../../../js/canvas-text-cube/r1/canvas-text-cube.js'
+   '../../../js/canvas-text-cube/r1/canvas-text-cube.js',
+   '../../../js/curve/r0/curve.js'
 ];
 // init
 VIDEO.init = function(sm, scene, camera){
-    //-------- ----------
-    // HELPERS
-    //-------- ----------
-    // just a short hand for THREE.QuadraticBezierCurve3
-    const QBC3 = function(x1, y1, z1, x2, y2, z2, x3, y3, z3){
-        let vs = x1;
-        let ve = y1;
-        let vc = z1;
-        if(arguments.length === 9){
-            vs = new THREE.Vector3(x1, y1, z1);
-            ve = new THREE.Vector3(x2, y2, z2);
-            vc = new THREE.Vector3(x3, y3, z3);
-        }
-        return new THREE.QuadraticBezierCurve3( vs, vc, ve );
-    };
-    // QBDelta helper using QBC3
-    // this works by giving deltas from the point that is half way between
-    // the two start and end points rather than a direct control point for x3, y3, and x3
-    const QBDelta = function(x1, y1, z1, x2, y2, z2, x3, y3, z3) {
-        const vs = new THREE.Vector3(x1, y1, z1);
-        const ve = new THREE.Vector3(x2, y2, z2);
-        // deltas
-        const vDelta = new THREE.Vector3(x3, y3, z3);
-        const vc = vs.clone().lerp(ve, 0.5).add(vDelta);
-        const curve = QBC3(vs, ve, vc);
-        return curve;
-    };
-    // QBV3Array
-    const QBV3Array = function(data) {
-        const v3Array = [];
-        data.forEach( ( a ) => {
-            const curve = QBDelta.apply(null, a.slice(0, a.length - 1))
-            v3Array.push( curve.getPoints( a[9]) );
-        })
-        return v3Array.flat();
-    };
     //-------- ----------
     // BACKGROUND
     //-------- ----------
@@ -56,6 +19,15 @@ VIDEO.init = function(sm, scene, camera){
     grid.material.linewidth = 3;
     scene.add( grid );
     //-------- ----------
+    // PATHS
+    //-------- ----------
+    const v3Array_campos = curveMod.QBV3Array([
+        [8,8,8, 7,-2,-7,    2,0,0,      20],
+        [7,-2,-7, -8,4,0,   0,0,0,      25],
+        [-8,4,0, 8,8,8,     0,0,0,      50]
+    ]);
+    scene.add( curveMod.debugPoints( v3Array_campos ) );
+    //-------- ----------
     // TEXT CUBE
     //-------- ----------
     const textCube = scene.userData.textCube = CanvasTextCube.create({
@@ -65,11 +37,11 @@ VIDEO.init = function(sm, scene, camera){
         lineColor: 'rgba(0,100,0,0.8)',
         lineCount: 9,
         lines: [
-            ['template2', 64, 17, 14, 'white'],
+            ['template2-video4', 64, 17, 14, 'white'],
             ['', 64, 32, 14, 'white'],
             ['', 64, 47, 14, 'white'],
             ['( r140 dd/mm/yyyy )', 64, 70, 12, 'gray'],
-            ['video3', 64, 100, 10, 'gray']
+            ['video1', 64, 100, 10, 'gray']
         ]
     });
     scene.add(textCube);
@@ -109,17 +81,6 @@ VIDEO.init = function(sm, scene, camera){
     //-------- ----------
     // A MAIN SEQ OBJECT
     //-------- ----------
-    const v3Array_campos = QBV3Array([
-        [8,8,8, 7,-2,-7,    2,0,0,      20],
-        [7,-2,-7, -8,4,0,   0,0,0,      25],
-        [-8,4,0, 8,8,8,     0,0,0,      50]
-    ]);
-    // PATH DEBUG POINTS
-    const points_debug = new THREE.Points(
-        new THREE.BufferGeometry().setFromPoints(v3Array_campos),
-        new THREE.PointsMaterial({size: 0.25, color: new THREE.Color(0,1,0)})
-    );
-    scene.add(points_debug);
     // start options for main seq object
     const opt_seq = {
         fps: 30,
@@ -157,9 +118,10 @@ VIDEO.init = function(sm, scene, camera){
             camera.lookAt(0, 0, 0);
         }
     };
+
     // SEQ 2 - ...
     opt_seq.objects[2] = {
-        secs: 5,
+        secs: 27,
         v3Paths: [
             { key: 'campos', array: v3Array_campos, lerp: true }
         ],
@@ -167,17 +129,6 @@ VIDEO.init = function(sm, scene, camera){
             // camera
             seq.copyPos('campos', camera);
             camera.lookAt(0, 0, 0);
-        }
-    };
-    // SEQ 2 - ...
-    opt_seq.objects[3] = {
-        secs: 20,
-        update: function(seq, partPer, partBias){
-            // camera
-            camera.position.set(8 - 16 * partBias, 8, 8);
-            camera.lookAt(0, 0, 0);
-            const b2 = seq.getSinBias(1, true);
-            camera.zoom = 1 + 7 * b2;
         }
     };
     const seq = scene.userData.seq = seqHooks.create(opt_seq);
