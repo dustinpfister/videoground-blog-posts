3b980c35db64a019f14e3bd7167511060174d061
dustinpfister <dustin.pfister@gmail.com>
2022-11-30 13:40:18 -0500

    clean up code

--- videos/threejs-object3d-visible/videos/video1.js
+++ videos/threejs-object3d-visible/videos/video1.js
@@ -7,80 +7,80 @@ VIDEO.scripts = [
 ];
 // init
 VIDEO.init = function(sm, scene, camera){
-//-------- ----------
-// HELPERS
-//-------- ----------
-// make a single box
-const makeBox = () => {
-    const box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-    return box;
-};
-// make a group
-const makeGroup = (count) => {
-    let i = 0;
-    const group = new THREE.Group();
-    while(i < count){
-        const mesh = makeBox();
-        group.add( mesh );
-        i += 1;
-    }
-    return group;
-};
-// set a group as a ring with options
-const setGroupAsRing = (group, opt) => {
-    opt = opt || {};
-    opt.alpha = opt.alpha === undefined ? 0 : opt.alpha;
-    opt.y = opt.y === undefined ? 0 : opt.y;
-    opt.scale = opt.scale === undefined ? 1 : opt.scale;
-    opt.radius = opt.radius === undefined ? 5 : opt.radius;
-    opt.radianOffset = opt.radianOffset === undefined ? 0 : opt.radianOffset;
-    opt.toggleAlpha = opt.toggleAlpha === undefined ? 0.10 : opt.toggleAlpha;
-    const len = group.children.length;
-    group.children.forEach( (mesh, i) => {
-        const alpha_mesh = i / len;
-        const radian = Math.PI * 2 * alpha_mesh + opt.radianOffset * Math.PI * 2;
-        mesh.position.x = Math.cos(radian) * opt.radius;
-        mesh.position.z = Math.sin(radian) * opt.radius;
-        mesh.position.y = opt.y;
-        // VISIBLE BASED ON i AS WELL AS opt.alpha
-        const a = opt.alpha % opt.toggleAlpha;
-        const b = a < opt.toggleAlpha / 2 ? 0 : 1;
-        const c = (i + b) % 2;
-        mesh.visible = true;
-        if(c){
-            mesh.visible = false;
+    //-------- ----------
+    // HELPERS
+    //-------- ----------
+    // make a single box
+    const makeBox = () => {
+        const box = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshNormalMaterial());
+        return box;
+    };
+    // make a group
+    const makeGroup = (count) => {
+        let i = 0;
+        const group = new THREE.Group();
+        while(i < count){
+            const mesh = makeBox();
+            group.add( mesh );
+            i += 1;
         }
-        // scale
-        mesh.scale.set(1, 1, 1).multiplyScalar(opt.scale)
-        // look at group
-        mesh.lookAt(group.position);
+        return group;
+    };
+    // set a group as a ring with options
+    const setGroupAsRing = (group, opt) => {
+        opt = opt || {};
+        opt.alpha = opt.alpha === undefined ? 0 : opt.alpha;
+        opt.y = opt.y === undefined ? 0 : opt.y;
+        opt.scale = opt.scale === undefined ? 1 : opt.scale;
+        opt.radius = opt.radius === undefined ? 5 : opt.radius;
+        opt.radianOffset = opt.radianOffset === undefined ? 0 : opt.radianOffset;
+        opt.toggleAlpha = opt.toggleAlpha === undefined ? 0.10 : opt.toggleAlpha;
+        const len = group.children.length;
+        group.children.forEach( (mesh, i) => {
+            const alpha_mesh = i / len;
+            const radian = Math.PI * 2 * alpha_mesh + opt.radianOffset * Math.PI * 2;
+            mesh.position.x = Math.cos(radian) * opt.radius;
+            mesh.position.z = Math.sin(radian) * opt.radius;
+            mesh.position.y = opt.y;
+            // VISIBLE BASED ON i AS WELL AS opt.alpha
+            const a = opt.alpha % opt.toggleAlpha;
+            const b = a < opt.toggleAlpha / 2 ? 0 : 1;
+            const c = (i + b) % 2;
+            mesh.visible = true;
+            if(c){
+                mesh.visible = false;
+            }
+            // scale
+            mesh.scale.set(1, 1, 1).multiplyScalar(opt.scale)
+            // look at group
+            mesh.lookAt(group.position);
+        });
+    };
+    //-------- ----------
+    // OBJECTS
+    //-------- ----------
+    // box helpers
+    scene.add( new THREE.GridHelper(10, 10));
+    const rings = new THREE.Group();
+    let ri = 0;
+    while(ri < 10){
+        rings.add( makeGroup(10) );
+        ri += 1;
+    }
+    scene.add(rings);
+    // add box helpers
+    rings.children.forEach( (r, ri) => {
+        r.children.forEach( (m, mi) => {
+            const box = new THREE.BoxHelper( m, 0xffff00 );
+            box.material.transparent = true;
+            box.material.opacity = 0.25;
+            box.material.linewidth = 3;
+            box.name = 'box_' + ri + '_' + mi;
+            scene.add(box);
+        });
     });
-}
-//-------- ----------
-// OBJECTS
-//-------- ----------
-// box helpers
-scene.add( new THREE.GridHelper(10, 10));
-const rings = new THREE.Group();
-let ri = 0;
-while(ri < 10){
-    rings.add( makeGroup(10) );
-    ri += 1;
-}
-scene.add(rings);
-// add box helpers
-rings.children.forEach( (r, ri) => {
-    r.children.forEach( (m, mi) => {
-        const box = new THREE.BoxHelper( m, 0xffff00 );
-        box.material.transparent = true;
-        box.material.opacity = 0.25;
-        box.material.linewidth = 3;
-        box.name = 'box_' + ri + '_' + mi;
-        scene.add(box);
-    });
-});
     //-------- ----------
     // BACKGROUND
     //-------- ----------
@@ -136,7 +136,7 @@ rings.children.forEach( (r, ri) => {
                 update: function(seq, partPer, partBias){
                     // text cube
                     textCube.visible = true;
-                    textCube.material.opacity = 1.0;
+                    textCube.material.opacity = 0.8;
                 }
             },
             {
@@ -146,7 +146,7 @@ rings.children.forEach( (r, ri) => {
                     textCube.visible = true;
                     textCube.position.set(6, 0.8 + 1 * partPer, 0);
                     textCube.rotation.y = Math.PI * 2 * partPer;
-                    textCube.material.opacity = 1.0 - partPer;
+                    textCube.material.opacity = 0.8 - 0.8 * partPer;
                 }
             }
         ]
@@ -161,27 +161,24 @@ rings.children.forEach( (r, ri) => {
             textCube.visible = false;
             camera.position.set(8, 1, 0);
             camera.zoom = 1;
-
-   const a1 = seq.per;
-   rings.children.forEach((r, ri)=>{
-       const a_r = ri / rings.children.length;
-       const a_r2 = Math.abs(0.5 - a_r) / 0.5;
-       const a3 = Math.sin( Math.PI * 0.25 * a_r);
-       setGroupAsRing(r, {
-           radius: 5 - 5 * a_r2,
-           radianOffset: Math.PI * 0.25 * a_r,
-           y: 5 - 10 * a3,
-           scale: 0.75 - 0.5 * a_r2,
-           alpha: a1
-       });
-       // update box helpers
-       r.children.forEach((m, mi) => {
-           const box = scene.getObjectByName('box_' + ri + '_' + mi);
-           box.update();
-       })
-   });
-
-
+            const a1 = seq.per;
+            rings.children.forEach((r, ri)=>{
+                const a_r = ri / rings.children.length;
+                const a_r2 = Math.abs(0.5 - a_r) / 0.5;
+                const a3 = Math.sin( Math.PI * 0.25 * a_r);
+                setGroupAsRing(r, {
+                    radius: 5 - 5 * a_r2,
+                    radianOffset: Math.PI * 0.25 * a_r,
+                    y: 5 - 10 * a3,
+                    scale: 0.75 - 0.5 * a_r2,
+                    alpha: a1
+                });
+                // update box helpers
+                r.children.forEach((m, mi) => {
+                    const box = scene.getObjectByName('box_' + ri + '_' + mi);
+                    box.update();
+                })
+            });
         },
         afterObjects: function(seq){
             camera.updateProjectionMatrix();
