03665ac473f702a2f82dbcf50bc255e8cdc84478
dustinpfister <dustin.pfister@gmail.com>
2023-02-22 10:52:47 -0500

    video2-edges file started for threejs-wireframe

---
+++ videos/threejs-wireframe/videos/video2-edges.js
@@ -0,0 +1,153 @@
+// video2-edges.js - for threejs-wireframe
+VIDEO.scripts = [
+   '../../../js/sequences-hooks/r2/sequences-hooks.js',
+   '../../../js/canvas/r2/lz-string.js',
+   '../../../js/canvas/r2/canvas.js',
+   '../../../js/curve/r1/curve.js',
+   '../../../js/canvas-text-cube/r1/canvas-text-cube.js'
+];
+// init
+VIDEO.init = function(sm, scene, camera){
+//-------- ----------
+// GEOMETRY
+//-------- ----------
+const boxGeo = new THREE.BoxGeometry(1.50, 1.50, 1.50),
+edgeGeo = new THREE.EdgesGeometry(boxGeo);
+//-------- ----------
+// LINE
+//-------- ----------
+const line = new THREE.LineSegments(
+    edgeGeo,
+    new THREE.LineBasicMaterial({
+        color: new THREE.Color('white'),
+        linewidth: 3
+    })
+);
+scene.add(line);
+    //-------- ----------
+    // BACKGROUND - using canvas2 and lz-string to create a background texture
+    //-------- ----------
+    const canObj = canvasMod.create({
+        size: 512,
+        draw: 'grid_palette',
+        palette: ['#000000', '#1f1f1f', '#00ffff'],
+        dataParse: 'lzstring64',
+        state: { w: 8, h: 5, data: 'AwGlEYyzNCVgpcmPit1mqvTsg===' }
+    });
+    // can use LZString to compress and decompress
+    //console.log( LZString.decompressFromBase64('AwGlEYyzNCVgpcmPit1mqvTsg===') );
+    // I want to repeat the texture
+    const texture = canObj.texture;
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(32, 24);
+    scene.background = texture;
+    //-------- ----------
+    // GRID
+    //-------- ----------
+    //const grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    //grid.material.linewidth = 3;
+    //scene.add( grid );
+    //-------- ----------
+    // TEXT CUBE
+    //-------- ----------
+    const textCube = scene.userData.textCube = CanvasTextCube.create({
+        bg: '#000000',
+        size: 128,
+        lineWidth: 7,
+        lineColor: 'rgba(0,100,0,0.8)',
+        lineCount: 9,
+        lines: [
+            ['Wireframe mode', 64, 17, 13, 'white'],
+            ['And Edge Geometry', 64, 32, 13, 'white'],
+            ['in threejs', 64, 47, 13, 'white'],
+            ['( r146 Feb/22/2023 )', 64, 70, 12, 'gray'],
+            ['video2', 64, 100, 10, 'gray']
+        ]
+    });
+    scene.add(textCube);
+    //-------- ----------
+    // A SEQ FOR TEXT CUBE
+    //-------- ----------
+    const seq_textcube = seqHooks.create({
+        setPerValues: false,
+        fps: 30,
+        beforeObjects: function(seq){
+            const textCube = scene.userData.textCube;
+            textCube.rotation.y = 0;
+            textCube.position.set(6, 0.8, 0);
+            textCube.visible = false;
+            textCube.material.transparent = true;
+            textCube.material.opacity = 0.0;
+        },
+        objects: [
+            {
+                per: 0,
+                update: function(seq, partPer, partBias){
+                    // text cube
+                    textCube.visible = true;
+                    textCube.material.opacity = 0.8;
+                }
+            },
+            {
+                per: 0.75,
+                update: function(seq, partPer, partBias){
+                    // move up text cube
+                    textCube.visible = true;
+                    textCube.position.set(6, 0.8 + 1 * partPer, 0);
+                    textCube.rotation.y = Math.PI * 2 * partPer;
+                    textCube.material.opacity = 0.8 - 0.8 * partPer;
+                }
+            }
+        ]
+    });
+    //-------- ----------
+    // A MAIN SEQ OBJECT
+    //-------- ----------
+    // start options for main seq object
+    const opt_seq = {
+        fps: 30,
+        beforeObjects: function(seq){
+            textCube.visible = false;
+            camera.position.set(8, 1, 0);
+            camera.zoom = 1;
+        },
+        afterObjects: function(seq){
+            camera.updateProjectionMatrix();
+        },
+        objects: []
+    };
+    // SEQ 0 - ...
+    opt_seq.objects[0] = {
+        secs: 3,
+        update: function(seq, partPer, partBias){
+            // textcube
+            if(seq.partFrame < seq.partFrameMax){
+               seqHooks.setFrame(seq_textcube, seq.partFrame, seq.partFrameMax);
+            }
+            // camera
+            //camera.position.set(-8, 4, -8);
+            camera.lookAt(0, 0, 0);
+            line.rotation.y = Math.PI * 2 * partPer;
+        }
+    };
+    // SEQ 1 - ...
+    opt_seq.objects[1] = {
+        secs: 27,
+        update: function(seq, partPer, partBias){
+            camera.zoom = 1 + 1.50 * partPer;
+            camera.lookAt(0, -0.05 * partPer, 0);
+            line.rotation.y = Math.PI * 8 * partPer;
+            line.rotation.z = Math.PI * 2 * partPer;
+        }
+    };
+    const seq = scene.userData.seq = seqHooks.create(opt_seq);
+    console.log('frameMax for main seq: ' + seq.frameMax);
+    sm.frameMax = seq.frameMax;
+};
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    const seq = scene.userData.seq;
+    seqHooks.setFrame(seq, sm.frame, sm.frameMax);
+};
+ 
