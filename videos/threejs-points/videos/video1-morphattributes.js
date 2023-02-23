9d4a22a5ed1a1514cdfdcef6febe22602376bd9d
dustinpfister <dustin.pfister@gmail.com>
2023-02-23 13:41:51 -0500

    video1 for threejs-points looks good

--- videos/threejs-points/videos/video1-morphattributes.js
+++ videos/threejs-points/videos/video1-morphattributes.js
@@ -14,7 +14,7 @@ VIDEO.init = function(sm, scene, camera){
 // GEOMETRY
 // base on this: https://github.com/mrdoob/three.js/blob/master/examples/webgl_morphtargets.html
 // ---------- ----------
-const geo = new THREE.BoxGeometry(2, 2, 2, 8, 8, 8);
+const geo = new THREE.BoxGeometry(2, 2, 2, 16, 16, 16);
 geo.morphAttributes.position = [];
 const pos = geo.attributes.position;
 const data_pos = [];
@@ -39,8 +39,9 @@ geo.setAttribute('color', new THREE.Float32BufferAttribute( data_color, 3 ));
 // ---------- ----------
 // MATERIAL, POINTS
 // ---------- ----------
-const material = new THREE.PointsMaterial({ size: 0.2, vertexColors: true });
+const material = new THREE.PointsMaterial({ size: 0.25, vertexColors: true });
 const points = new THREE.Points(geo, material);
+points.scale.set(3, 3, 3);
 scene.add(points);
 points.morphTargetInfluences[ 0 ] = 1;
 // ---------- ----------
@@ -70,25 +71,10 @@ const updatepoints = function(points, alpha){
     texture.repeat.set(32, 24);
     scene.background = texture;
     //-------- ----------
-    // CURVE PATHS - creating a curve path for the camera
-    //-------- ----------
-    const cp_campos = curveMod.QBCurvePath([
-        [8,1,0, 8,3,8,  5,2,5,    0]
-    ]);
-    //scene.add( curveMod.debugPointsCurve(cp_campos) )
-    //-------- ----------
-    // CURVE Alphas
-    //-------- ----------
-    const getCamPosAlpha = curveMod.getAlphaFunction({
-        type: 'curve2',
-        ac_points: [0,0.4,  0.6,-0.25,  1]
-    });
-    //scene.add( curveMod.debugAlphaFunction(getCamPosAlpha) )
-    //-------- ----------
     // GRID
     //-------- ----------
     const grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
-    grid.material.linewidth = 3;
+    grid.material.linewidth = 5;
     scene.add( grid );
     //-------- ----------
     // TEXT CUBE
@@ -128,7 +114,7 @@ const updatepoints = function(points, alpha){
                 update: function(seq, partPer, partBias){
                     // text cube
                     textCube.visible = true;
-                    textCube.material.opacity = 0.8;
+                    textCube.material.opacity = 0.9;
                 }
             },
             {
@@ -138,7 +124,7 @@ const updatepoints = function(points, alpha){
                     textCube.visible = true;
                     textCube.position.set(6, 0.8 + 1 * partPer, 0);
                     textCube.rotation.y = Math.PI * 2 * partPer;
-                    textCube.material.opacity = 0.8 - 0.8 * partPer;
+                    textCube.material.opacity = 0.9 - 0.9 * partPer;
                 }
             }
         ]
@@ -159,28 +145,93 @@ const updatepoints = function(points, alpha){
         },
         objects: []
     };
-    // SEQ 0 - ...
+    // SEQ 0 - Text cube
     opt_seq.objects[0] = {
         secs: 3,
         update: function(seq, partPer, partBias){
+            // POINTS
+            updatepoints(points, seq.getSinBias(1));
             // textcube
             if(seq.partFrame < seq.partFrameMax){
                seqHooks.setFrame(seq_textcube, seq.partFrame, seq.partFrameMax);
             }
             // camera
-            //camera.position.set(-8, 4, -8);
+            camera.position.set(8, 1, 0);
             camera.lookAt(0, 0, 0);
         }
     };
-    // SEQ 1 - ...
+    // SEQ 1 - morph up and down a few times, zoom out
     opt_seq.objects[1] = {
-        secs: 27,
+        secs: 4,
         update: function(seq, partPer, partBias){
-            const a1 = getCamPosAlpha(partPer);
-            camera.position.copy( cp_campos.getPoint(a1) );
+            // POINTS
+            updatepoints(points, seq.getSinBias(4));
+            // camera
+            // camera
+            camera.zoom = 1 - 0.5 * partPer;
+            camera.position.set(8, 1, 0);
             camera.lookAt(0, 0, 0);
         }
     };
+    // SEQ 2 - rotate camera around object in cube state
+    opt_seq.objects[2] = {
+        secs: 5,
+        update: function(seq, partPer, partBias){
+            // POINTS
+            updatepoints(points, 0);
+            // camera
+            camera.zoom = 0.5;
+            const e = new THREE.Euler();
+            e.y = Math.PI * 2 * partPer;
+            camera.position.set(8, 1, 0).applyEuler(e);
+            camera.lookAt(0, 0, 0);
+        }
+    };
+
+    // SEQ 3 - rotate camera around object and morph to sphere
+    opt_seq.objects[3] = {
+        secs: 5,
+        update: function(seq, partPer, partBias){
+            // POINTS
+            updatepoints(points, partPer);
+            // camera
+            camera.zoom = 0.5;
+            const e = new THREE.Euler();
+            e.y = Math.PI * 2 * partPer;
+            camera.position.set(8, 1, 0).applyEuler(e);
+            camera.lookAt(0, 0, 0);
+        }
+    };
+    // SEQ 4 - rotate camera around object and in sphere state
+    opt_seq.objects[4] = {
+        secs: 5,
+        update: function(seq, partPer, partBias){
+            // POINTS
+            updatepoints(points, 1);
+            // camera
+            camera.zoom = 0.5;
+            const e = new THREE.Euler();
+            e.y = Math.PI * 2 * partPer;
+            camera.position.set(8, 1, 0).applyEuler(e);
+            camera.lookAt(0, 0, 0);
+        }
+    };
+    // SEQ 5 - rotate camera around object and in morph in and out
+    opt_seq.objects[5] = {
+        secs: 8,
+        update: function(seq, partPer, partBias){
+            // POINTS
+            updatepoints(points, 1 - seq.getSinBias(4));
+            // camera
+            camera.zoom = 0.5;
+            const e = new THREE.Euler();
+            e.y = Math.PI * 2 * partPer;
+            e.x = Math.PI * 0.25 * partPer;
+            camera.position.set(8, 1, 0).applyEuler(e);
+            camera.lookAt(0, 0, 0);
+        }
+    };
+
     const seq = scene.userData.seq = seqHooks.create(opt_seq);
     console.log('frameMax for main seq: ' + seq.frameMax);
     sm.frameMax = seq.frameMax;
