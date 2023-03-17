762bbef25698ec3ab3f6537ff4eb4ce901564b9e
dustinpfister <dustin.pfister@gmail.com>
2023-03-17 12:59:44 -0400

    adjusted opacity for video5-canvas-r2 of template2

--- templates/template2-seqhooks/videos/video5-canvas-r2.js
+++ templates/template2-seqhooks/videos/video5-canvas-r2.js
@@ -89,7 +89,7 @@ VIDEO.init = function(sm, scene, camera){
                 update: function(seq, partPer, partBias){
                     // text cube
                     textCube.visible = true;
-                    textCube.material.opacity = 0.8;
+                    textCube.material.opacity = 0.9;
                 }
             },
             {
@@ -99,7 +99,7 @@ VIDEO.init = function(sm, scene, camera){
                     textCube.visible = true;
                     textCube.position.set(6, 0.8 + 1 * partPer, 0);
                     textCube.rotation.y = Math.PI * 2 * partPer;
-                    textCube.material.opacity = 0.8 - 0.8 * partPer;
+                    textCube.material.opacity = 0.9 - 0.9 * partPer;
                 }
             }
         ]
